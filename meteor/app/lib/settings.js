5312b6728a87ff071373842ca412c9e79ebfb80f
Jeff Bryner <jeff@jeffbryner.com>
2014-03-05 12:49:04 -0800

    add settings for kibana urls

--- meteor/app/lib/settings.js
+++ meteor/app/lib/settings.js
@@ -1,16 +1,16 @@
 //configuration settings
 
 elasticsearch={
-address:"http://esservername:9200/",
+address:"http://servername:9200/",
 healthurl:"_cluster/health",
 docstatsurl:"_stats/docs"
 }
 
+
 mozdef={
-    rootURL:"http://mozdefservername",
-    port:"3000",
-    ldapKibana:"http://kibanaservername:portnumber/pathtokibana",
-    eventsKibana:"http://kibanaservername:portnumber/pathtokibana",
-    alertsKibana:"http://kibanaservername:portnumber/pathtokibana",
-    
+    rootURL:"https://servername",
+    port:"443",
+    ldapKibana:"http://servername:9090/#/dashboard/elasticsearch/mozdef%20ldap%20dashboard",
+    eventsKibana:"http://servername:9090/index.html#/dashboard/elasticsearch/Logstash%20Style%20Search",
+    alertsKibana:"http://servername:9090/index.html#/dashboard/elasticsearch/Alerts"
 }
