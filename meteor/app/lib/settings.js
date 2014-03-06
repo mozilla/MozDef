3191139b2a887726596b6f68c956f662c7c920a4
Jeff Bryner <jeff@jeffbryner.com>
2014-03-06 15:33:35 -0800

    sane settings for getting started with meteor

--- meteor/app/lib/settings.js
+++ meteor/app/lib/settings.js
@@ -1,15 +1,15 @@
 //configuration settings
 
 elasticsearch={
-address:"http://servername:9200/",
+address:"http://localhost:9200/",
 healthurl:"_cluster/health",
 docstatsurl:"_stats/docs"
 }
 
 
 mozdef={
-    rootURL:"https://servername",
-    port:"443",
+    rootURL:"http://localhost",
+    port:"3000",
     ldapKibana:"http://servername:9090/#/dashboard/elasticsearch/mozdef%20ldap%20dashboard",
     eventsKibana:"http://servername:9090/index.html#/dashboard/elasticsearch/Logstash%20Style%20Search",
     alertsKibana:"http://servername:9090/index.html#/dashboard/elasticsearch/Alerts"
