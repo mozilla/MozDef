b02b294ddaf69dd9b48b634d5bbd6c3ab6b853b7
Anthony Verez <averez@mozilla.com>
2014-03-29 19:17:29 -0700

    more sane default settings for meteor

--- meteor/app/lib/settings.js
+++ meteor/app/lib/settings.js
@@ -1,18 +1,18 @@
 //configuration settings
 
 elasticsearch={
-address:"http://servername:9200/",
+address:"http://localhost:9200/",
 healthurl:"_cluster/health",
 docstatsurl:"_stats/docs"
 }
 
 
 mozdef={
-    rootURL:"https://servername",
+    rootURL:"https://localhost",
     port:"443",
-    ldapKibana:"http://servername:9090/#/dashboard/elasticsearch/mozdef%20ldap%20dashboard",
-    eventsKibana:"http://servername:9090/index.html#/dashboard/elasticsearch/Logstash%20Style%20Search",
-    alertsKibana:"http://servername:9090/index.html#/dashboard/elasticsearch/Alerts",
-    ldapLoginDataURL:"https://rest.servername:8444/ldapLogins/",
-    alertDataURL:"https://rest.servername:8444/alerts/"    
+    ldapKibana:"http://localhost:9090/#/dashboard/elasticsearch/mozdef%20ldap%20dashboard",
+    eventsKibana:"http://localhost:9090/index.html#/dashboard/elasticsearch/Logstash%20Style%20Search",
+    alertsKibana:"http://localhost:9090/index.html#/dashboard/elasticsearch/Alerts",
+    ldapLoginDataURL:"https://localhost:8444/ldapLogins/", // rest server
+    alertDataURL:"https://localhost:8444/alerts/" // rest server
 }
