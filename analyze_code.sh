efe4bebff79bc644d6b858777db441dfe6f0053b
ba2150 <bjorn.arnelid@alten.se>
2014-04-04 15:51:10 +0200

    Added pymetrics codechecking...

--- analyze_code.sh
+++ analyze_code.sh
@@ -1,34 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 # Automated static code analysis for Python and JavaScript
 # Python tools pylint, pep8, pyflakes
-# Javascrip tools jshint
 NOW=$(date +%Y%m%d_%H%M%S)
 
 echo "Creating folder $NOW"
+mkdir -p results/$NOW
 mkdir -p results/$NOW
 cd results/$NOW
 
 echo "Analyzing python code"
 if hash pyflakes 2>/dev/null; then
-    echo "Running pyflakes"
-    pyflakes ../.. > pyflakes.txt
+	echo "Running pyflakes"
+	pyflakes ../.. > pyflakes.txt
 else
-    echo "Could not find pyflakes"
+	echo "Could not find pyflakes"
 fi
 
 if hash pylint 2>/dev/null; then
-    echo "Running pylint"
-    pylint ../../*/*.py --output-format=parseable > pylint.txt
+	echo "Running pylint"
+	pylint ../../*/*.py --output-format=parseable > pylint.txt
 else
-    echo "Could not find pylint"
+	echo "Could not find pylint"
 fi
 
 if hash pep8 2>/dev/null; then
-    echo "Running pep8"
-    pep8 ../../*/*.py > pep8.txt
+	echo "Running pep8"
+	pep8 ../../*/*.py > pep8.txt
 else
-    echo "Could not find pep8"
+	echo "Could not find pep8"
 fi
-
+if hash pymetrics 2>/dev/null; then
+	echo "Running pymetrics"
+	pymetrics ../../*/*.py > pymetrics.txt
+else
+	echo "Could not find pymetrics"
+fi
 # TBD Analyze JavaScript Code
 
