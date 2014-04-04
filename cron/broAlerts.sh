2bfb1a2d9b3e136cdc0643bfaa02f345655c2ac8
Jeff Bryner <jeff@jeffbryner.com>
2014-04-03 21:11:11 -0700

    explicitly define the config file

--- cron/broAlerts.sh
+++ cron/broAlerts.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 source  /home/mozdef/envs/mozdef/bin/activate
-/home/mozdef/envs/mozdef/cron/broAlerts.py
-
+/home/mozdef/envs/mozdef/cron/broAlerts.py -c /home/mozdef/envs/mozdef/cron/broAlerts.conf
