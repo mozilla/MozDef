3dd054b1f24eca8c855207953415d26dc41e85c4
Anthony Verez <averez@mozilla.com>
2014-04-14 15:56:48 -0700

    averez-heka-snippets: Copy the true bro-lua-bro-intel (Closes #38)

--- examples/heka-lua-bro-intel/brointel.lua
+++ examples/heka-lua-bro-intel/brointel.lua
@@ -6,24 +6,28 @@ local elem = lpeg.C((1-sep)^0)
 local grammar = -lpeg.P"#" * lpeg.Ct(elem * (sep * elem)^0) -- ignore comment, split on tabs, return as table
 
 local msg = {
-    Type = "bronotice_log",
+    Type = "brointel_log",
     Logger = "nsm",
     Fields = {
         -- Initializing our fields
         ['details.ts'] = nil,
         ['details.uid'] = nil,
-        ['details.orig_h'] = nil,
-        ['details.orig_p'] = nil,
-        ['details.resp_h'] = nil,
-        ['details.resp_p'] = nil,
-        ['details.proto'] = nil,
-        ['details.note'] = nil,
-        ['details.msg'] = nil,
-        ['details.sub'] = nil,
+        ['details.sourceipaddress'] = nil,
+        ['details.sourceport'] = nil,
+        ['details.destinationipaddress'] = nil,
+        ['details.destinationport'] = nil,
+        ['details.fuid'] = nil,
+        ['details.filemimetype'] = nil,
+        ['details.filedesc'] = nil,
+        ['details.seenindicator'] = nil,
+        ['details.seenwhere'] = nil,
+        ['details.seenindicatortype'] = nil,
+        ['details.sources'] = nil,
+        ['summary'] = nil,
         summary = nil,
         severity = "NOTICE",
-        category = "bronotice",
-        tags = "nsm,bro,notice"
+        category = "brointel",
+        tags = "nsm,bro,intel"
     }
 }
 
@@ -33,19 +37,20 @@ function process_message()
     local matches = grammar:match(log)
     if not matches then return -1 end
 
-    -- populating our fields
     msg.Fields['details.ts'] = matches[1]
     msg.Fields['details.uid'] = matches[2]
     msg.Fields['details.sourceipaddress'] = matches[3]
     msg.Fields['details.sourceport'] = matches[4]
     msg.Fields['details.destinationipaddress'] = matches[5]
     msg.Fields['details.destinationport'] = matches[6]
-    msg.Fields['details.proto'] = matches[10]
-    msg.Fields['details.note'] = matches[11]
-    msg.Fields['details.msg'] = matches[12]
-    msg.Fields['details.sub'] = matches[13]
-    -- Our summary is the concatenation of other fields
-    msg.Fields['summary'] = string.format("%s %s %s", msg.Fields['details.note'], msg.Fields['details.msg'], msg.Fields['details.sub'])
+    msg.Fields['details.fuid'] = matches[7]
+    msg.Fields['details.filemimetype'] = matches[8]
+    msg.Fields['details.filedesc'] = matches[9]
+    msg.Fields['details.seenindicator'] = matches[10]
+    msg.Fields['details.seenwhere'] = matches[11]
+    msg.Fields['details.seenindicatortype'] = matches[12]
+    msg.Fields['details.sources'] = matches[13]
+    msg.Fields['summary'] = "Bro intel match: " .. msg.Fields['details.seenindicator']
     inject_message(msg)
     return 0
 end
