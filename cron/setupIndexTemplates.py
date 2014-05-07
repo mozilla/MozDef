77451e6372e55dd09c73f60ae5026afe4b13d225
Anthony Verez <averez@mozilla.com>
2014-05-07 13:35:37 -0700

    averez-102-esmapping: add multifield type with raw subfield for the index template

--- cron/setupIndexTemplates.py
+++ cron/setupIndexTemplates.py
@@ -19,115 +19,193 @@ from configlib import getConfig, OptionParser
 
 def esPutTemplates():
     eventstemplate = r'''
-    {
-        "template" : "events*",
-        "mappings" : {
-          "event" : {
-            "properties" : {
-              "category" : {
-                "index" : "not_analyzed",
-                "type" : "string"
-              },
-              "details" : {
-                "properties" : {
-                  "destinationipaddress" : {
-                    "type" : "ip"
-                  },
-                  "destinationport" : {
-                    "type" : "string"
-                  },
-                  "email" : {
-                    "index" : "not_analyzed",
-                    "type" : "string"
-                  },                  
-                  "dn" : {
-                    "type" : "string"
-                  },
-                  "hostname" : {
-                    "type" : "string"
-                  },
-                  "msg" : {
-                    "type" : "string"
-                  },
-                  "note" : {
-                    "type" : "string"
-                  },
-                  "processid" : {
-                    "type" : "string"
-                  },
-                  "program" : {
-                    "type" : "string"
-                  },
-                  "protocol" : {
-                    "type" : "string"
-                  },
-                  "result" : {
-                    "type" : "string"
-                  },
-                  "source" : {
-                    "type" : "string"
-                  },
-                  "sourceipaddress" : {
-                    "type" : "ip"
-                  },
-                  "sourceport" : {
-                    "type" : "string"
-                  },
-                  "srcip" : {
-                    "type" : "ip"
-                  },
-                  "sub" : {
-                    "type" : "string"
-                  },
-                  "success" : {
-                    "type" : "boolean"
-                  },
-                  "timestamp" : {
-                    "type" : "string"
-                  },
-                  "ts" : {
-                    "type" : "string"
-                  },
-                  "uid" : {
-                    "type" : "string"
-                  }
-                }
-              },
-              "eventsource" : {
-                "type" : "string"
-              },
-              "hostname" : {
-                "type" : "string"
-              },
-              "processid" : {
-                "type" : "string"
-              },
-              "receivedtimestatmp" : {
-                "type" : "date",
-                "format" : "dateOptionalTime"
-              },
-              "severity" : {
-                "type" : "string"
-              },
-              "summary" : {
-                "type" : "string"
-              },
-              "tags" : {
-                "index" : "not_analyzed",
-                "type" : "string"
-              },
-              "timestamp" : {
-                "type" : "date",
-                "format" : "dateOptionalTime"
-              },
-              "utctimestamp" : {
-                "type" : "date",
-                "format" : "dateOptionalTime"
-              }
-            }
-          }
-        }
-    }
+	{
+	    "template":"events*",
+	    "mappings":{
+	        "event":{
+	            "_all" : {"enabled" : true},
+	            "dynamic_templates" : [ {
+	                "string_fields" : {
+	                    "match" : "*",
+	                    "match_mapping_type" : "string",
+	                    "mapping" : {
+	                        "type" : "multi_field",
+	                        "fields" : {
+	                            "{name}" : {"type": "string", "index" : "analyzed", "omit_norms" : true },
+	                            "raw" : {"type": "string", "index" : "not_analyzed", "ignore_above" : 256}
+	                        }
+	                    }
+	                }
+	            } ],
+	            "properties":{
+	                "category":{
+	                  "index":"not_analyzed",
+	                  "type":"string"
+	                },
+	                "details":{
+	                  "properties":{
+	                    "destinationipaddress":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "destinationipaddress" : {"type" : "ip"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "destinationport":{
+	                      "type":"string"
+	                    },
+	                    "dn":{
+	                      "type":"string"
+	                    },
+	                    "hostname":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "hostname": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "msg":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "msg": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "note":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "note": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "processid":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "processid": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "program":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "program": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "protocol":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "protocol": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "result":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "result": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "source":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "source": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "sourceipaddress":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "sourceipaddress": {"type": "ip"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "sourceport":{
+	                      "type":"string"
+	                    },
+	                    "sub":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "sub": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    },
+	                    "success":{
+	                      "type":"boolean"
+	                    },
+	                    "timestamp":{
+	                      "type":"string"
+	                    },
+	                    "ts":{
+	                      "type":"string"
+	                    },
+	                    "uid":{
+	                      "type" : "multi_field",
+	                      "fields" : {
+	                        "uid": {"type": "string"},
+	                        "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                      }
+	                    }
+	                  }
+	                },
+	                "eventsource":{
+	                  "type" : "multi_field",
+	                  "fields" : {
+	                    "eventsource": {"type": "string"},
+	                    "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                  }
+	                },
+	                "hostname":{
+	                  "type" : "multi_field",
+	                  "fields" : {
+	                    "hostname": {"type": "string"},
+	                    "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                  }
+	                },
+	                "processid":{
+	                  "type" : "multi_field",
+	                  "fields" : {
+	                    "processid": {"type": "string"},
+	                    "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                  }
+	                },
+	                "receivedtimestatmp":{
+	                  "type":"date",
+	                  "format":"dateOptionalTime"
+	                },
+	                "severity":{
+	                  "type" : "multi_field",
+	                  "fields" : {
+	                    "severity": {"type": "string"},
+	                    "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                  }
+	                },
+	                "summary":{
+	                  "type" : "multi_field",
+	                  "fields" : {
+	                    "summary": {"type": "string"},
+	                    "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                  }
+	                },
+	                "tags":{
+	                  "type" : "multi_field",
+	                  "fields" : {
+	                    "tags": {"type": "string"},
+	                    "raw" : {"type" : "string", "index" : "not_analyzed"}
+	                  }
+	                },
+	                "timestamp":{
+	                  "type":"date",
+	                  "format":"dateOptionalTime"
+	                },
+	                "utctimestamp":{
+	                  "type":"date",
+	                  "format":"dateOptionalTime"
+	                }
+	            }
+	        }
+	    }
+	}
     '''
     url = '{0}/_template/eventstemplate'.format(options.esservers[0])
     r = requests.put(url=url, data=eventstemplate)
@@ -137,59 +215,82 @@ def esPutTemplates():
         print('Problem putting events template %r' % r)
 
     alertstemplate = r'''
-    {
-        "template" : "alerts*",
-        "mappings" : {
-              "alert" : {
-                "properties" : {
-                  "timestamp" : {
-                    "format" : "dateOptionalTime",
-                    "type" : "date"
-                  },
-                  "tags" : {
-                    "type" : "string"
-                  },
-                  "summary" : {
-                    "type" : "string"
-                  },
-                  "receivedtimestatmp" : {
-                    "format" : "dateOptionalTime",
-                    "type" : "date"
-                  },
-                  "category" : {
-                    "index" : "not_analyzed",
-                    "type" : "string"
-                  },
-                  "events" : {
-                    "properties" : {
-                      "id" : {
-                        "type" : "string"
-                      },
-                      "index" : {
-                        "type" : "string"
-                      },
-                      "type" : {
-                        "type" : "string"
-                      }
-                    }
-                  },
-                  "eventsource" : {
-                    "type" : "string"
-                  },
-                  "hostname" : {
-                    "type" : "string"
-                  },
-                  "severity" : {
-                    "type" : "string"
-                  },
-                  "utctimestamp" : {
-                    "format" : "dateOptionalTime",
-                    "type" : "date"
-                  }
-                }
-              }
-        }
-    }
+	{
+	  "template":"alerts*",
+	  "mappings":{
+	    "alert":{
+	      "properties":{
+	        "timestamp":{
+	          "format":"dateOptionalTime",
+	          "type":"date"
+	        },
+	        "tags":{
+	          "type" : "multi_field",
+	          "fields" : {
+	            "tags": {"type": "string"},
+	            "raw" : {"type" : "string", "index" : "not_analyzed"}
+	          }
+	        },
+	        "summary":{
+	          "type" : "multi_field",
+	          "fields" : {
+	            "summary": {"type": "string"},
+	            "raw" : {"type" : "string", "index" : "not_analyzed"}
+	          }
+	        },
+	        "receivedtimestatmp":{
+	          "format":"dateOptionalTime",
+	          "type":"date"
+	        },
+	        "category":{
+	          "type" : "multi_field",
+	          "fields" : {
+	            "category": {"type": "string"},
+	            "raw" : {"type" : "string", "index" : "not_analyzed"}
+	          }
+	        },
+	        "events":{
+	          "properties":{
+	            "id":{
+	              "type":"string"
+	            },
+	            "index":{
+	              "type":"string"
+	            },
+	            "type":{
+	              "type":"string"
+	            }
+	          }
+	        },
+	        "eventsource":{
+	          "type" : "multi_field",
+	          "fields" : {
+	            "eventsource": {"type": "string"},
+	            "raw" : {"type" : "string", "index" : "not_analyzed"}
+	          }
+	        },
+	        "hostname":{
+	          "type" : "multi_field",
+	          "fields" : {
+	            "hostname": {"type": "string"},
+	            "raw" : {"type" : "string", "index" : "not_analyzed"}
+	          }
+	        },
+	        "severity":{
+	          "type" : "multi_field",
+	          "fields" : {
+	            "severity": {"type": "string"},
+	            "raw" : {"type" : "string", "index" : "not_analyzed"}
+	          }
+	        },
+	        "utctimestamp":{
+	          "format":"dateOptionalTime",
+	          "type":"date"
+	        }
+	      }
+	    }
+	  }
+	}
     '''
 
     url = '{0}/_template/alertstemplate'.format(options.esservers[0])
