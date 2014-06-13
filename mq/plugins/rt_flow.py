860e29f15cc63a61c13a51c334fc295310966741
Anthony Verez <averez@mozilla.com>
2014-06-13 11:01:09 -0700

    averez-rtflow-plugin: also parse RT_FLOW_SESSION_CREATE messages

--- mq/plugins/rt_flow.py
+++ mq/plugins/rt_flow.py
@@ -15,16 +15,17 @@ class message(object):
            set the priority if you have a preference for order of plugins to run.
            0 goes first, 100 is assumed/default if not sent
         '''
-        self.registration = ['RT_FLOW']
+        self.registration = ['rt_flow']
         self.priority = 5
         self.deny_regex = re.compile(r'%-RT_FLOW_SESSION_DENY: session denied (?P<src>([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))/(?P<srcport>[0-9]+)->(?P<dst>([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))/(?P<dstport>[0-9]+) (?P<service>\S+) (?P<proto>[0-9]+)\((?P<prototype>[0-9]+)\) (?P<policy>\S+) (?P<srczone>\S+) (?P<dstzone>\S+) UNKNOWN UNKNOWN N/A\(N/A\) (?P<interface>\S+)(\n)?')
+        self.create_regex = re.compile(r'%-RT_FLOW_SESSION_CREATE: session created (?P<src>([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))/(?P<srcport>[0-9]+)->(?P<dst>([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))/(?P<dstport>[0-9]+) (?P<service>\S+) (?P<src2>([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))/(?P<srcport2>[0-9]+)->(?P<dst2>([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))/(?P<dstport2>[0-9]+) (?P<srcnatrule>\S+) (?P<dstnatrule>\S+) (?P<protocol>\S+) (?P<policy>\S+) (?P<srczone>\S+) (?P<dstzone>\S+) (?P<sessionid>\S+) N/A\(N/A\) (?P<interface>\S+)(\n)?')
 
     def onMessage(self, message, metadata):
         if 'details' in message.keys():
             if 'program' in message['details'].keys():
             	if 'RT_FLOW' == message['details']['program']:
             		msg_unparsed = message['summary']
-            		if len(msg_unparsed) > 23 and msg_unparsed[:23] == '%-RT_FLOW_SESSION_DENY:':
+            		if msg_unparsed.startswith('%-RT_FLOW_SESSION_DENY:'):
             			deny_search = re.search(self.deny_regex, msg_unparsed)
             			if deny_search:
             				message['details']['src'] = deny_search.group('src')
@@ -38,5 +39,21 @@ class message(object):
             				message['details']['srczone'] = deny_search.group('srczone')
             				message['details']['dstzone'] = deny_search.group('dstzone')
             				message['details']['interface'] = deny_search.group('interface')
+            		if msg_unparsed.startswith('%-RT_FLOW_SESSION_CREATE:'):
+            			create_search = re.search(self.create_regex, msg_unparsed)
+            			if create_search:
+            				message['details']['src'] = create_search.group('src')
+            				message['details']['srcport_int'] = create_search.group('srcport')
+            				message['details']['dst'] = create_search.group('dst')
+            				message['details']['dstport_int'] = create_search.group('dstport')
+            				message['details']['service'] = create_search.group('service')
+            				message['details']['srcnatrule'] = create_search.group('srcnatrule')
+            				message['details']['dstnatrule'] = create_search.group('dstnatrule')
+            				message['details']['protocol'] = create_search.group('protocol')
+            				message['details']['policy'] = create_search.group('policy')
+            				message['details']['srczone'] = create_search.group('srczone')
+            				message['details']['dstzone'] = create_search.group('dstzone')
+            				message['details']['sessionid_int'] = create_search.group('sessionid')
+            				message['details']['interface'] = create_search.group('interface')
 
         return (message, metadata)
