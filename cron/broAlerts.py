da70b8206b4d9b01b1119b5f36ac5b253b63870e
Jeff Bryner <jeff@jeffbryner.com>
2014-06-28 06:32:38 -0700

    updated bro alert searches

--- cron/broAlerts.py
+++ cron/broAlerts.py
@@ -91,105 +91,149 @@ def alertToES(es,alertDict):
     except pyes.exceptions.NoServerAvailable:
         logger.error('Elastic Search server could not be reached, check network connectivity')
 
-def esSearch(es,begindateUTC=None, enddateUTC=None):
-    resultsList=list()
-    if begindateUTC is None:
-        begindateUTC=toUTC(datetime.now() - timedelta(minutes=60))
-    if enddateUTC is None:
-        enddateUTC= toUTC(datetime.now())
-    try:
-        #search for events within the date range that haven't already been alerted (i.e. given an alerttimestamp)
-        qDate=pyes.RangeQuery(qrange=pyes.ESRange('utctimestamp',from_value=begindateUTC,to_value=enddateUTC))
-        qType=pyes.TermFilter('_type','event')
-        qEvents=pyes.TermsFilter('category',['brointel'])
-        qalerted=pyes.ExistsFilter('alerttimestamp')
-        qdetails=pyes.ExistsFilter('details')
-        qindicator=pyes.ExistsFilter('seenindicator')
-        pyesresults=es.search(pyes.ConstantScoreQuery(pyes.BoolFilter(must=[qType,qDate,qEvents,qdetails,qindicator],must_not=[qalerted])),size=1000)
-        #uncomment for debugging to recreate alerts for events that already have an alerttimestamp
-        #results=es.search(pyes.ConstantScoreQuery(pyes.BoolFilter(must=[qcloud,qDate,qEvents])))
-        #logger.debug(results.count())
-        
-        #correlate any matches by the seenindicator field.
-        #make a simple list of indicator values that can be counted/summarized by Counter
-        resultsIndicators=list()
-        
-        #bug in pyes..capture results as raw list or it mutates after first access:
-        #copy the hits.hits list as our resusts, which is the same as the official elastic search library returns.
-        results=pyesresults._search_raw()['hits']['hits']
-        for r in results:
-            resultsIndicators.append(r['_source']['details']['seenindicator'])
+def esBroIntelEvents():
+    begindateUTC= toUTC(datetime.now() - timedelta(minutes=30))
+    enddateUTC= toUTC(datetime.now())        
+    #search for events within the date range that haven't already been alerted (i.e. given an alerttimestamp)
+    qDate=pyes.RangeQuery(qrange=pyes.ESRange('utctimestamp',from_value=begindateUTC,to_value=enddateUTC))
+    qType=pyes.TermFilter('_type','event')
+    qEvents=pyes.TermsFilter('category',['brointel'])
+    qalerted=pyes.ExistsFilter('alerttimestamp')
+    q=pyes.ConstantScoreQuery(pyes.MatchAllQuery())
+    q.filters.append(pyes.BoolFilter(
+        must=[qType, 
+              qDate,
+              qEvents,
+              pyes.ExistsFilter('seenindicator')
+        ],
+        must_not=[
+        qalerted
+        ]))
+    return q
 
-        #use the list of tuples ('indicator',count) to create a dictionary with:
-        #indicator,count,es records
-        #and add it to a list to return.
-        indicatorList=list()
+
+def esBroXSSEvents():
+    begindateUTC= toUTC(datetime.now() - timedelta(minutes=30))
+    enddateUTC= toUTC(datetime.now())
+    qDate = pyes.RangeQuery(qrange=pyes.ESRange('utctimestamp', from_value=begindateUTC, to_value=enddateUTC))
+    qType = pyes.TermFilter('_type', 'event')
+    qEvents = pyes.TermFilter("category","bro_xss_log")
+    qalerted = pyes.ExistsFilter('alerttimestamp')
+    q=pyes.ConstantScoreQuery(pyes.MatchAllQuery())
+    q.filters.append(pyes.BoolFilter(
+        must=[qType, 
+              qDate,
+              qEvents,
+              pyes.ExistsFilter('uri')
+        ],
+        must_not=[
+        qalerted
+        ]))
+    return q
+
+def esRunSearch(es, query, aggregateField, detailLimit=5):
+    try:
+        pyesresults = es.search(query, size=1000, indices='events,events-previous')
+        # logger.debug(results.count())
+
+        # correlate any matches by the aggregate field.
+        # make a simple list of indicator values that can be counted/summarized by Counter
+        resultsIndicators = list()
+
+        # bug in pyes..capture results as raw list or it mutates after first access:
+        # copy the hits.hits list as our results, which is the same as the official elastic search library returns.
+        results = pyesresults._search_raw()['hits']['hits']
+        for r in results:
+            resultsIndicators.append(r['_source']['details'][aggregateField])
+
+        # use the list of tuples ('indicator',count) to create a dictionary with:
+        # indicator,count,es records
+        # and add it to a list to return.
+        indicatorList = list()
         for i in Counter(resultsIndicators).most_common():
-            idict=dict(indicator=i[0],count=i[1],events=[])
+            idict = dict(indicator=i[0], count=i[1], events=[])
             for r in results:
-                if r['_source']['details']['seenindicator'].encode('ascii','ignore')==i[0]:
-                    idict['events'].append(r)
+                if r['_source']['details'][aggregateField].encode('ascii', 'ignore') == i[0]:
+                    # copy events detail into this correlation up to our detail limit
+                    if len(idict['events'])<detailLimit:
+                        idict['events'].append(r)
             indicatorList.append(idict)
         return indicatorList
 
     except pyes.exceptions.NoServerAvailable:
-        logger.error('Elastic Search server could not be reached, check network connectivity')
-        
-def createAlerts(es,indicatorCounts):
+        logger.error('Elastic Search server could not be reached, check network connectivity')  
+
+
+def createAlerts(es, indicatorCounts, threshold, description):
     '''given a list of dictionaries:
         count: X
         indicator: sometext
         events: list of pyes results matching the indicator
-        
+
         1) create a summary alert with detail of the events
         2) update the events with an alert timestamp so they are not included in further alerts
     '''
     try:
-        if len(indicatorCounts)>0:
+        if len(indicatorCounts) > 0:
             for i in indicatorCounts:
-                alert=dict(utctimestamp=toUTC(datetime.now()).isoformat(),severity='NOTICE',summary='',category='bro',tags=['bro','intel'],eventsource=[],events=[])
-                for e in i['events']:
-                    alert['events'].append(
-                        dict(documentindex=e['_index'],
-                             documenttype=e['_type'],
-                             documentsource=e['_source'], 
-                             documentid=e['_id']))                    
-                alert['severity']='NOTICE'
-                alert['summary']=('{0} matches for indicator {1} '.format(i['count'],i['indicator']))
-                for e in i['events']:
-                    #append the relevant events in text format to avoid errant ES issues.
-                    #should be able to just set eventsource to i['events'] but different versions of ES 1.0 complain
-                    alert['eventsource'].append(flattenDict(e))
-                #alert['eventsource']=i['events']
-                logger.debug(alert['summary'])
-                logger.debug(alert['events'])
-                logger.debug(alert)
-                
-                #save alert to alerts index, update events index with alert ID for cross reference
-                alertResult=alertToES(es,alert)
-                
-                logger.debug(alertResult)
-                #for each event in this list of indicatorCounts
-                #update with the alertid/index
-                #and update the alerttimestamp on the event itself so it's not re-alerted
-                for e in i['events']:
-                    if 'alerts' not in e['_source'].keys():
-                        e['_source']['alerts']=[]
-                    e['_source']['alerts'].append(dict(index=alertResult['_index'],type=alertResult['_type'],id=alertResult['_id']))
-                    e['_source']['alerttimestamp']=toUTC(datetime.now()).isoformat()
-                    es.update(e['_index'],e['_type'],e['_id'],document=e['_source'])
-                
-                alertToMessageQueue(alert)
+                if i['count'] > threshold:
+                    alert = dict(utctimestamp=toUTC(datetime.now()).isoformat(), severity='NOTICE', summary='', category='bro', tags=['bro'], eventsource=[], events=[])
+                    for e in i['events']:
+                        alert['events'].append(
+                            dict(documentindex=e['_index'],
+                                 documenttype=e['_type'],
+                                 documentsource=e['_source'],
+                                 documentid=e['_id']))
+                    alert['severity'] = 'NOTICE'
+                    alert['summary'] = ('{0} {1}: {2}'.format(i['count'], description, i['indicator']))
+                    # append first X source IPs
+                    alert['summary'] += ' sample sourceips: '
+                    for e in i['events'][0:3]:
+                        if 'sourceipaddress' in e['_source']['details'].keys():
+                            alert['summary'] += '{0} '.format(e['_source']['details']['sourceipaddress'])
+                    for e in i['events']:
+                        # append the relevant events in text format to avoid errant ES issues.
+                        # should be able to just set eventsource to i['events'] but different versions of ES 1.0 complain
+                        alert['eventsource'].append(flattenDict(e))
+    
+                    logger.debug(alert['summary'])
+                    logger.debug(alert['events'])
+                    logger.debug(alert)
+    
+                    # save alert to alerts index, update events index with alert ID for cross reference
+                    alertResult = alertToES(es, alert)
+    
+                    ##logger.debug(alertResult)
+                    # for each event in this list of indicatorCounts
+                    # update with the alertid/index
+                    # and update the alerttimestamp on the event itself so it's not re-alerted
+                    for e in i['events']:
+                        if 'alerts' not in e['_source'].keys():
+                            e['_source']['alerts'] = []
+                        e['_source']['alerts'].append(dict(index=alertResult['_index'], type=alertResult['_type'], id=alertResult['_id']))
+                        e['_source']['alerttimestamp'] = toUTC(datetime.now()).isoformat()
+    
+                        es.update(e['_index'], e['_type'], e['_id'], document=e['_source'])
+    
+                    alertToMessageQueue(alert)
     except ValueError as e:
-        logger.error("Exception %r when creating alerts "%e)
+        logger.error("Exception %r when creating alerts " % e)
         
 def main():
     logger.debug('starting')
     logger.debug(options)
     es=pyes.ES((list('{0}'.format(s) for s in options.esservers)))
-    #see if we have matches.
-    indicatorCounts=esSearch(es)
-    createAlerts(es,indicatorCounts)
+    # search for brointel
+    #indicatorCounts=esSearch(es)
+    #createAlerts(es,indicatorCounts)
+    
+    indicatorCounts=esRunSearch(es,esBroIntelEvents(),'seenindicator', 50)
+    createAlerts(es,indicatorCounts, 5, 'bro intel match')
+    
+    # search for xss events
+    indicatorCounts=esRunSearch(es,esBroXSSEvents(),'cluster_client_ip', 50)
+    createAlerts(es,indicatorCounts, 5, 'bro xss')    
+    
     logger.debug('finished')
 
 def initConfig():
