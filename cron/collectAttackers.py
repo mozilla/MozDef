457d1ccf4ca7b68926b5209cdea701fcbebf6b50
Jeff Bryner <jeff@jeffbryner.com>
2014-08-05 13:37:43 -0700

    don't assume default timezone

--- cron/collectAttackers.py
+++ cron/collectAttackers.py
@@ -47,10 +47,12 @@ def initLogger():
         logger.addHandler(sh)
 
 
-def toUTC(suspectedDate, localTimeZone="US/Pacific"):
+def toUTC(suspectedDate, localTimeZone=None):
     '''make a UTC date out of almost anything'''
     utc = pytz.UTC
     objDate = None
+    if localTimeZone is None:
+        localTimeZone=options.defaulttimezone    
     if type(suspectedDate) in (str, unicode):
         objDate = parse(suspectedDate, fuzzy=True)
     elif type(suspectedDate) == datetime:
@@ -345,6 +347,8 @@ def main():
 
 
 def initConfig():
+    #change this to your default timezone
+    options.defaulttimezone=getConfig('defaulttimezone','UTC',options.configfile)
     # output our log to stdout or syslog
     options.output = getConfig('output', 'stdout', options.configfile)
     # syslog hostname
