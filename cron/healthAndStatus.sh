844cc0e7df6e4a9054a8e4e0c0bee94e0a7d888b
Jeff Bryner <jeff@jeffbryner.com>
2014-12-09 09:35:44 -0800

    add event stats to the health/status

--- cron/healthAndStatus.sh
+++ cron/healthAndStatus.sh
@@ -11,3 +11,4 @@
 source  /home/mozdef/envs/mozdef/bin/activate
 /home/mozdef/envs/mozdef/cron/healthAndStatus.py -c /home/mozdef/envs/mozdef/cron/healthAndStatus.conf
 /home/mozdef/envs/mozdef/cron/healthToMongo.py -c /home/mozdef/envs/mozdef/cron/healthToMongo.conf
+/home/mozdef/envs/mozdef/cron/eventStats.py -c /home/mozdef/envs/mozdef/cron/eventStats.conf
