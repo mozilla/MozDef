2d57f8838089ded197fa0a8215bb3361053fa943
Julien Vehent <julien@linuxwall.info>
2014-12-16 16:13:49 -0500

    complianceitems mozdef plugin, take 1

---
+++ mq/plugins/complianceitems.py
@@ -0,0 +1,76 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2014 Mozilla Corporation
+#
+# Contributors:
+# Julien Vehent jvehent@mozilla.com
+
+import hashlib
+import sys
+
+
+class message(object):
+    def __init__(self):
+        self.registration = ['complianceitems']
+        self.priority = 20
+
+    def validate(message):
+        """
+            Validate that a compliance item has all the necessary keys
+        """
+        for key in ['target', 'policy', 'check', 'compliance',
+                    'link', 'timestamp']:
+            if key not in message.keys():
+                return False
+        for key in ['level', 'name', 'url']:
+            if key not in message['policy'].keys():
+                return False
+        for key in ['description', 'location', 'name', 'test']:
+            if key not in message['check'].keys():
+                return False
+        for key in ['type', 'value']:
+            if key not in message['check']['test'].keys():
+                return False
+        return True
+
+    def cleanup_item(message):
+        ci = {}
+        ci['target'] = message['target']
+        ci['policy'] = {}
+        ci['policy']['level'] = message['policy']['level']
+        ci['policy']['name'] = message['policy']['name']
+        ci['policy']['url'] = message['policy']['url']
+        ci['check'] = {}
+        ci['check']['description'] = message['check']['description']
+        ci['check']['location'] = message['check']['location']
+        ci['check']['name'] = message['check']['name']
+        ci['check']['test'] = {}
+        ci['check']['test']['type'] = message['check']['test']['type']
+        ci['check']['test']['value'] = message['check']['test']['value']
+        ci['compliance'] = message['compliance']
+        ci['link'] = message['link']
+        ci['timestamp'] = message['timestamp']
+        return ci
+
+    def onMessage(self, message, metadata):
+        """
+            The complianceitems plugin is called when an event
+            is posted with a doctype 'complianceitems'.
+            Compliance items are stored in the complianceitems
+            index, with doctype last_known_state
+        """
+        if metadata['doc_type'] == 'complianceitems':
+            if not validate(message):
+                sys.stdout.write('error: invalid format for complianceitem ' +
+                                 message['id'])
+                return (None, None)
+            ci = cleanup_item(message)
+            docidstr = 'complianceitems'
+            docidstr += complianceitem['check']['ref']
+            docidstr += complianceitem['check']['test']['value']
+            docidstr += complianceitem['target']
+            metadata['id'] = hashlib.md5(docidstr).hexdigest()
+            metadata['doc_type'] = 'last_known_state'
+            metadata['index'] = 'complianceitems'
+        return (message, metadata)
