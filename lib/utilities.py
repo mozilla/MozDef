6a7e3369999fa91fa0b03f24084421f1061f9836
Brandon Myers <bmyers@mozilla.com>
2016-07-13 16:16:11 -0500

    Add toUTC support for short and long epoch
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- lib/utilities.py
+++ lib/utilities.py
@@ -8,18 +8,18 @@ def toUTC(suspectedDate, localTimeZone='UTC'):
     utc=pytz.UTC
     objDate=None
 
-    if type(suspectedDate) in (str,unicode):
-        objDate=parse(suspectedDate,fuzzy=True)
-    elif type(suspectedDate)==datetime:
-        objDate=suspectedDate
+    if type(suspectedDate) == datetime:
+        objDate = suspectedDate
+    elif str(suspectedDate).isdigit():
+        # epoch? but seconds/milliseconds/nanoseconds (lookin at you heka)
+        epochDivisor = int(str(1) + '0'*(len(str(suspectedDate)) % 10))
+        objDate = datetime.fromtimestamp(float(suspectedDate/epochDivisor))
+    elif type(suspectedDate) in (str, unicode):
+        objDate = parse(suspectedDate, fuzzy=True)
 
     if objDate.tzinfo is None:
         objDate=pytz.timezone(localTimeZone).localize(objDate)
-        objDate=utc.normalize(objDate)
-    else:
-        objDate=utc.normalize(objDate)
 
-    if objDate is not None:
-        objDate=utc.normalize(objDate)
+    objDate=utc.normalize(objDate)
 
     return objDate
