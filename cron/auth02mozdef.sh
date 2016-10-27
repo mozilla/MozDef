38ee234650edb882a28c7b5ab90879e7daf81029
Brandon Myers <bmyers@mozilla.com>
2016-10-27 15:24:02 -0500

    Add auth02mozdef cron files
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

---
+++ cron/auth02mozdef.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2016 Mozilla Corporation
+# Author: gdestuynder@mozilla.com
+
+
+source  /home/mozdef/envs/mozdef/bin/activate
+/home/mozdef/envs/mozdef/cron/auth02mozdef.py
