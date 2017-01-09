377e14ce2623e6f576c4dfc5cc0688774fdddfc6
Brandon Myers <bmyers@mozilla.com>
2017-01-09 15:26:40 -0600

    Remove unused cron alert scripts
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- cron/auditDAlerts.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-source  /opt/mozdef/envs/mozdef/bin/activate
-/opt/mozdef/envs/mozdef/cron/auditDAlerts.py -c /opt/mozdef/envs/mozdef/cron/auditDAlerts.conf
-/opt/mozdef/envs/mozdef/cron/auditDFileAlerts.py -c /opt/mozdef/envs/mozdef/cron/auditDAlerts.conf
