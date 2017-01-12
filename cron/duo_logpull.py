dc7437ca63c3b29d18b9eba4c2c9a9fa8f7c2f91
Brandon Myers <bmyers@mozilla.com>
2017-01-11 19:51:29 -0600

    Fix duo logpull state file
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- cron/duo_logpull.py
+++ cron/duo_logpull.py
@@ -71,9 +71,8 @@ def process_events(mozmsg, duo_events, etype, state):
 
 
 def main():
-    state_location = os.path.dirname(sys.argv[0]) + '/' + options.statepath
     try:
-        state = pickle.load(open(state_location, 'rb'))
+        state = pickle.load(open(options.statepath, 'rb'))
     except IOError:
         # Oh, you're new.
         state = {'administration': 0, 'authentication': 0, 'telephony': 0}
@@ -90,7 +89,7 @@ def main():
     state = process_events(mozmsg, duo.get_authentication_log(mintime=state['authentication']+1), 'authentication', state)
     state = process_events(mozmsg, duo.get_telephony_log(mintime=state['telephony']+1), 'telephony', state)
 
-    pickle.dump(state, open(state_location, 'wb'))
+    pickle.dump(state, open(options.statepath, 'wb'))
 
 
 def initConfig():
