3933cc67bb4f7ddce7e6034ce2a2ff7a0a38d93e
Brandon Myers <bmyers@mozilla.com>
2017-01-26 15:33:38 -0600

    Add open port alert to config
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- alerts/open_port_violation.py
+++ alerts/open_port_violation.py
@@ -10,7 +10,7 @@
 # Brandon Myers bmyers@mozilla.com
 
 from lib.alerttask import AlertTask
-from query_models import SearchQuery, TermMatch, PhraseMatch, TermsMatch
+from query_models import SearchQuery, TermMatch, PhraseMatch
 
 
 class AlertOpenPortViolation(AlertTask):
@@ -18,8 +18,8 @@ class AlertOpenPortViolation(AlertTask):
         search_query = SearchQuery(hours=4)
 
         search_query.add_must([
-            TermMatch('_type', 'event'), 
-            PhraseMatch('tag', 'open_port_policy_violation'),
+            TermMatch('_type', 'event'),
+            PhraseMatch('tags', 'open_port_policy_violation'),
         ])
 
         self.filtersManual(search_query)
@@ -39,10 +39,10 @@ class AlertOpenPortViolation(AlertTask):
         tags = ['open_port_policy_violation']
         severity = 'CRITICAL'
 
-        summary = ('{0} unauthorized open port(s) on {1}'.format(aggreg['count'], aggreg['value']))
-        # hosts = self.mostCommon(aggreg['allevents'], '_source.details.hostname')
-        # for i in hosts[:5]:
-        #     summary += ' {0} ({1} hits)'.format(i[0], i[1])
+        summary = ('{0} unauthorized open port(s) on {1} ('.format(aggreg['count'], aggreg['value']))
+        for event in aggreg['events'][:5]:
+            summary += str(event['_source']['details']['destinationport']) + ' '
+        summary += ')'
 
         # Create the alert object based on these properties
         return self.createAlertDict(summary, category, tags, aggreg['events'], severity)
