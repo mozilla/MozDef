f935917a01e37d1cbaab56b135958f006aa14f88
Jonathan Claudius <jclaudius@mozilla.com>
2017-01-26 15:42:59 -0500

    Fix copyright

--- alerts/open_port_violation.py
+++ alerts/open_port_violation.py
@@ -3,7 +3,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# Copyright (c) 2014 Mozilla Corporation
+# Copyright (c) 2017 Mozilla Corporation
 #
 # Contributors:
 # Jonathan Claudius jclaudius@mozilla.com
