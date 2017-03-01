a06961635869a6ab0bb255af856dc7bac1c0a6f9
Brandon Myers <bmyers@mozilla.com>
2017-03-01 15:27:08 -0600

    Add check if ip list contains less than certain ips
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- cron/update_ip_list.py
+++ cron/update_ip_list.py
@@ -17,7 +17,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib
 from utilities.logger import logger, initLogger
 
 
-def fetch_ip_list_contents(aws_key_id, aws_secret_key, s3_bucket, ip_list_filename):
+def fetch_ip_list(aws_key_id, aws_secret_key, s3_bucket, ip_list_filename):
     logger.debug("Fetching ip list from s3")
     s3 = boto.connect_s3(
         aws_access_key_id=aws_key_id,
@@ -25,9 +25,11 @@ def fetch_ip_list_contents(aws_key_id, aws_secret_key, s3_bucket, ip_list_filena
     )
     bucket = s3.get_bucket(s3_bucket)
     ip_list_key = bucket.lookup(ip_list_filename)
-    return ip_list_key.get_contents_as_string().rstrip()
+    contents = ip_list_key.get_contents_as_string().rstrip()
+    return contents.split("\n")
 
-def save_ip_list(save_path, ip_list_contents):
+def save_ip_list(save_path, ips):
+    ip_list_contents = '\n'.join(ips)
     logger.debug("Saving ip list")
     if os.path.isfile(save_path):
         logger.debug("Overwriting ip list file in " + str(save_path))
@@ -39,7 +41,9 @@ def save_ip_list(save_path, ip_list_contents):
 def main():
     logger.debug('Starting')
     logger.debug(options)
-    ips = fetch_ip_list_contents(options.aws_access_key_id, options.aws_secret_access_key, options.aws_bucket_name, options.aws_document_key_name)
+    ips = fetch_ip_list(options.aws_access_key_id, options.aws_secret_access_key, options.aws_bucket_name, options.aws_document_key_name)
+    if len(ips) < options.ips_list_threshold:
+        raise LookupError('IP List contains less than ' + str(options.ips_list_threshold) + ' entries...something is probably up here.')
     save_ip_list(options.local_ip_list_path, ips)
 
 def initConfig():
@@ -54,6 +58,7 @@ def initConfig():
     options.aws_document_key_name=getConfig('aws_document_key_name','',options.configfile)
 
     options.local_ip_list_path = getConfig('local_ip_list_path', '', options.configfile)
+    options.ips_list_threshold = getConfig('ips_list_threshold', 20, options.configfile)
 
 
 if __name__ == '__main__':
