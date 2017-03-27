a5fc3020944c721ad3591075e4ede2f80cdb18fc
Brandon Myers <bmyers@mozilla.com>
2017-03-27 15:22:47 -0500

    Remove fake event generation in deadman alert
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- alerts/deadman.py
+++ alerts/deadman.py
@@ -15,19 +15,6 @@ from lib.alerttask import AlertTask
 from query_models import SearchQuery, TermMatch, PhraseMatch
 
 
-def fakeEvent():
-    # make a fake event
-    # mimicing some parameters since this isn't an ES event
-    # but should have an ES event metadata structure
-    # like _index, _type, etc.
-    event = dict()
-    event['_index'] = ''
-    event['_type'] = ''
-    event['_source'] = dict()
-    event['_id'] = ''
-    return event
-
-
 class broNSM(AlertTask):
     def main(self, *args, **kwargs):
         # call with hostlist=['host1','host2','host3']
@@ -60,17 +47,5 @@ class broNSM(AlertTask):
         summary = ('No {0} bro healthcheck events found the past 20 minutes'.format(hostname))
         url = "https://mana.mozilla.org/wiki/display/SECURITY/NSM+IR+procedures"
 
-        # make an event to attach to the alert
-        event = fakeEvent()
-        # attach our info about not having an event to _source:
-        # to mimic an ES document
-        event['_source']['category'] = 'deadman'
-        event['_source']['tags'] = ['bro']
-        event['_source']['severity'] = 'ERROR'
-        event['_source']['hostname'] = hostname
-        event['_source']['summary'] = summary
-        # serialize the filter to avoid datetime objects causing json problems.
-        event['_source']['details'] = dict(filter='{0}'.format(self.filter.serialize()))
-
         # Create the alert object based on these properties
-        return self.createAlertDict(summary, category, tags, [event], severity=severity, url=url)
+        return self.createAlertDict(summary, category, tags, [], severity=severity, url=url)
