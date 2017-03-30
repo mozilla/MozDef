f5423345051c086461ba287da83a5a2eb213c947
Michal Purzynski <michal@mozilla.com>
2017-03-30 12:50:08 -0700

    Bring the duo_authfail to the newest message format, several fixups.

--- alerts/duo_authfail.py
+++ alerts/duo_authfail.py
@@ -13,12 +13,12 @@ from query_models import SearchQuery, TermMatch, ExistsMatch, PhraseMatch
 
 class AlertDuoAuthFail(AlertTask):
     def main(self):
-        search_query = SearchQuery(minutes=30)
+        search_query = SearchQuery(minutes=2)
 
         search_query.add_must([
             TermMatch('_type', 'event'),
             TermMatch('category', 'event'),
-            ExistsMatch('details.ip'),
+            ExistsMatch('details.sourceipaddress'),
             ExistsMatch('details.username'),
             PhraseMatch('details.result', 'FRAUD'),
         ])
@@ -30,6 +30,7 @@ class AlertDuoAuthFail(AlertTask):
     # Set alert properties
     def onEvent(self, event):
         category = 'duosecurity'
+        tags = ['duosecurity']
         severity = 'WARNING'
         url = "https://mana.mozilla.org/wiki/display/SECURITY/IR+Procedure%3A+DuoSecurity"
 
@@ -37,12 +38,12 @@ class AlertDuoAuthFail(AlertTask):
         user = 'unknown'
         x = event['_source']
         if 'details' in x:
-            if 'ip' in x['details']:
-                sourceipaddress = x['details']['ip']
+            if 'sourceipaddress' in x['details']:
+                sourceipaddress = x['details']['sourceipaddress']
             if 'username' in x['details']:
                 user = x['details']['username']
 
         summary = 'Duo Authentication Failure: user {1} rejected and marked a Duo Authentication attempt from {0} as fraud'.format(sourceipaddress, user)
 
         # Create the alert object based on these properties
-        return self.createAlertDict(summary, category, [], [event], severity, url)
+        return self.createAlertDict(summary, category, tags, [event], severity, url)
---
+++ tests/alerts/test_duo_authfail.py
@@ -0,0 +1,122 @@
+from positive_alert_test_case import PositiveAlertTestCase
+from negative_alert_test_case import NegativeAlertTestCase
+
+from alert_test_suite import AlertTestSuite
+
+
+class TestAlertDuoAuthFail(AlertTestSuite):
+    alert_filename = "duo_authfail"
+
+    # This event is the default positive event that will cause the
+    # alert to trigger
+    default_event = {
+        "_type": "event",
+        "_source": {
+            "category": "event",
+            "summary": 'authentication FRAUD for you@somewhere.com',
+            "details": {
+                "sourceipaddress": "1.2.3.4",
+                "username": "you@somewhere.com",
+                "result": "FRAUD",
+            }
+        }
+    }
+
+    # This alert is the expected result from running this task
+    default_alert = {
+        "category": "duosecurity",
+        "tags": ['duosecurity'],
+        "severity": "WARNING",
+        "url": "https://mana.mozilla.org/wiki/display/SECURITY/IR+Procedure%3A+DuoSecurity",
+        "summary": "Duo Authentication Failure: user you@somewhere.com rejected and marked a Duo Authentication attempt from 1.2.3.4 as fraud",
+    }
+
+    test_cases = [
+        PositiveAlertTestCase(
+            description="Positive test case with good event",
+            events=[default_event],
+            expected_alert=default_alert
+        ),
+
+        PositiveAlertTestCase(
+            description="Positive test case with an event with somewhat old timestamp",
+            events=[
+                {
+                    "_source": {
+                        "utctimestamp": AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 1})
+                    }
+                }
+            ],
+            expected_alert=default_alert
+        ),
+
+        NegativeAlertTestCase(
+            description="Negative test case with bad event type",
+            events=[
+                {
+                    "_type": "wrongtype",
+                }
+            ],
+        ),
+
+        NegativeAlertTestCase(
+            description="Negative test case with bad category",
+            events=[
+                {
+                    "_source": {
+                        "category": "badcategory",
+                    }
+                }
+            ],
+        ),
+
+        NegativeAlertTestCase(
+            description="Negative test case with bad result",
+            events=[
+                {
+                    "_source": {
+                        "details": {
+                            "result": "SUCCESS",
+                        }
+                    }
+                }
+            ],
+        ),
+
+        NegativeAlertTestCase(
+            description="Negative test case without the details.sourceipaddress",
+            events=[
+                {
+                    "_source": {
+                        "details": {
+                            "sourceipaddress": None,
+                        }
+                    }
+                }
+            ],
+        ),
+
+        NegativeAlertTestCase(
+            description="Negative test case without the details.username",
+            events=[
+                {
+                    "_source": {
+                        "details": {
+                            "username": None,
+                        }
+                    }
+                }
+            ],
+        ),
+
+        NegativeAlertTestCase(
+            description="Negative test case with a wrong timestamp",
+            events=[
+                {
+                    "_source": {
+                        "utctimestamp": AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 3})
+                    }
+                }
+            ],
+        ),
+    ]
