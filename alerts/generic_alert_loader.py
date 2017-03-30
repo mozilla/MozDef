42d1178a8fb50c9e1320b938655435e4b20c4a60
Brandon Myers <bmyers@mozilla.com>
2017-03-30 14:47:45 -0500

    Modify generic alert loader with validation
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- alerts/generic_alert_loader.py
+++ alerts/generic_alert_loader.py
@@ -7,17 +7,58 @@
 #
 # Contributors:
 # kang@mozilla.com
+# bmyers@mozilla.com
 
 # TODO: Dont use query_models, nicer fixes for AlertTask
 
 from lib.alerttask import AlertTask
 from query_models import SearchQuery, TermMatch, QueryStringMatch
 import hjson
+import logging
 import sys
 import traceback
 import glob
 import os
 from configlib import getConfig, OptionParser
+
+# Minimum data needed for an alert (this is an example alert json)
+'''
+    {
+        # Lucene search string
+        'search_string': 'field1: matchingvalue and field2: matchingothervalue',
+
+        # ES Filters as such: [['field', 'value'], ['field', 'value']]
+        'filters': [],
+
+        # What to aggregate on if we get multiple matches?
+        'aggregation_key': 'summary',
+
+        # How long to search and aggregate for? The longer the slower.
+        # These defaults work well for alerts that basically don't *need*
+        # much aggregation
+        'threshold': {
+            'timerange_min': 5,
+            'count': 1
+        },
+
+        # This is the category that will show up in mozdef, and the severity
+        'alert_category': 'generic_alerts',
+        'alert_severity': 'INFO',
+
+        # This will show up as the alert text when it trigger
+        'summary': 'Example summary that shows up in the alert',
+
+        # This helps sorting out alerts, so it's nice if you fill this in
+        'tags': ['generic'],
+
+        # This is the alert documentation
+        'url': 'https://mozilla.org'
+    }
+'''
+
+
+logger = logging.getLogger()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 class DotDict(dict):
@@ -33,59 +74,24 @@ class DotDict(dict):
             self[key] = value
 
 
-def debug(msg):
-    sys.stderr.write(msg + "\n")
-
-
 class AlertGenericLoader(AlertTask):
-    # Minimum data needed for an alert (this is the alert schema)
-    # If a field is missing it will be filled in by these defaults
-    self.default_alert = DotDict({
-            # Lucene search string
-            'search_string': '',
-            # ES Filters as such: [['field', 'value'], ['field', 'value']]
-            'filters': [],
-            # What to aggregate on if we get multiple matches?
-            'aggregation_key': 'summary',
-            # How long to search and aggregate for? The longer the slower.
-            # These defaults work well for alerts that basically don't *need*
-            # much aggregation
-            'threshold': {
-                'timerange_min': 5,
-                'count': 1
-                },
-            # This is the category that will show up in mozdef, and the severity
-            'alert_category': 'generic_alerts',
-            'alert_severity': 'INFO',
-            # This will show up as the alert text when it trigger
-            'summary': '',
-            # This helps sorting out alerts, so it's nice if you fill this in
-            'tags': ['generic'],
-            # This is the alert documentation
-            'url': ''
-            })
-    # The above default alert also require these to be set
-    # by the alert json file. If it's not set, fail.
-    self.default_alert_required_fields = DotDict({
-        'search_string': '',
-        'summary': '',
-        'url': ''
-        })
+    required_fields = [
+        "search_string",
+        "filters",
+        "threshold",
+        "aggregation_key",
+        "alert_category",
+        "tags",
+        "alert_severity",
+        "summary",
+        "url",
+    ]
 
     def validate_alert(self, alert):
-        '''Validate the alert has required fields and fills in defaults where possible'''
-        new_alert = self.default_alert.copy()
-        for k in self.default_alert:
-            # If we have this field set, add it, else we keep the default
-            # Granted that its not a required field
-            if k in alert:
-                new_alert[k] = alert[k]
-            else:
-                # Take default, or fail if it was a required field
-                if k in self.default_alert_required_fields:
-                    debug('Your alert does not have the required field {}'.format(k))
-                    raise KeyError
-        return new_alert
+        for key in self.required_fields:
+            if key not in alert:
+                logger.error('Your alert does not have the required field {}'.format(key))
+                raise KeyError
 
     def load_configs(self):
         '''Load all configured rules'''
@@ -94,14 +100,12 @@ class AlertGenericLoader(AlertTask):
         files = glob.glob(rules_location + "/*.json")
         for f in files:
             with open(f) as fd:
-                # XXX Make a nicer try thing
                 try:
                     cfg = DotDict(hjson.load(fd))
-                    cfg = validate_alert(cfg)
+                    self.validate_alert(cfg)
                     self.configs.append(cfg)
-                except:
-                    traceback.print_exc(file=sys.stdout)
-                    debug("Loading rule file {} failed".format(f))
+                except Exception:
+                    logger.error("Loading rule file {} failed".format(f))
 
     def initConfiguration(self):
         myparser = OptionParser()
@@ -127,9 +131,9 @@ class AlertGenericLoader(AlertTask):
         for cfg in self.configs:
             try:
                 self.process_alert(cfg)
-            except:
+            except Exception:
                 traceback.print_exc(file=sys.stdout)
-                debug("Processing rule file {} failed".format(cfg.__str__()))
+                logger.error("Processing rule file {} failed".format(cfg.__str__()))
 
     def onAggregation(self, aggreg):
         # aggreg['count']: number of items in the aggregation, ex: number of failed login attempts
@@ -140,6 +144,22 @@ class AlertGenericLoader(AlertTask):
         severity = aggreg['config']['alert_severity']
         url = aggreg['config']['url']
 
-        summary = '{} ({}): {}'.format(aggreg['config']['summary'], aggreg['count'], aggreg['value'])
+        # Find all affected hosts
+        # Normally, the hostname data is in e.details.hostname so try that first,
+        # but fall back to e.hostname if it is missing, or nothing at all if there's no hostname! ;-)
+        hostnames = []
+        for e in aggreg['events']:
+            event_source = e['_source']
+            if 'details' in event_source and 'hostname' in event_source['details']:
+                hostnames.append(event_source['details']['hostname'])
+            elif 'hostname' in event_source:
+                hostnames.append(event_source['hostname'])
+
+        summary = '{} ({}): {} [{}]'.format(
+            aggreg['config']['summary'],
+            aggreg['count'],
+            aggreg['value'],
+            ','.join(hostnames)
+        )
 
         return self.createAlertDict(summary, category, tags, aggreg['events'], severity, url)
