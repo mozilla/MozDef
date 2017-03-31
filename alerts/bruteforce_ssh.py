14491ad7d034d3ba72ada976495c5e501331dfb9
Brandon Myers <bmyers@mozilla.com>
2017-03-31 10:02:57 -0500

    Add pentest server to ssh whitelist
    Signed-off-by: Brandon Myers <bmyers@mozilla.com>

--- alerts/bruteforce_ssh.py
+++ alerts/bruteforce_ssh.py
@@ -27,6 +27,8 @@ class AlertBruteforceSsh(AlertTask):
         search_query.add_must_not([
             PhraseMatch('summary', '10.22.75.203'),
             PhraseMatch('summary', '10.8.75.144'),
+            # bug 1350650
+            PhraseMatch('summary', '10.22.75.37'),
         ])
 
         self.filtersManual(search_query)
