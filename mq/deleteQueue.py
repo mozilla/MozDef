1c4fc1071ca647162a4a38548bf3bc820c1e6132
Brandon Myers <bmyers@mozilla.com>
2017-05-25 12:26:54 -0500

    Remove unused mq workers

--- mq/deleteQueue.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-import pika
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel=connection.channel()
-channel.queue_delete(queue=sys.argv[1])
-connection.close()
