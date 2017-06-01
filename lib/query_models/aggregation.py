fa8e3d4cc318d48f1e4c1ad6bc37d7777c8206ce
Brandon Myers <bmyers@mozilla.com>
2017-06-01 18:05:21 -0500

    Expand query_models into separate folder

---
+++ lib/query_models/aggregation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2017 Mozilla Corporation
+#
+# Contributors:
+# Brandon Myers bmyers@mozilla.com
+
+
+from elasticsearch_dsl import A
+
+
+def Aggregation(field_name, aggregation_size=20):
+    return A('terms', field=field_name, size=aggregation_size)
