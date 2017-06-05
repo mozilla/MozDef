a22f9fde47245259daa39ab7ee6e9c359c46c168
Brandon Myers <bmyers@mozilla.com>
2017-06-05 13:07:17 -0500

    Remove mozilla hostnames from meteor settings

--- meteor/app/lib/settings.js
+++ meteor/app/lib/settings.js
@@ -2,20 +2,21 @@
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
-Copyright (c) 2014 Mozilla Corporation
+Copyright (c) 2017 Mozilla Corporation
 
 Contributors:
 Jeff Bryner jbryner@mozilla.com
 Anthony Verez averez@mozilla.com
+Brandon Myers bmyers@mozilla.com
 */
 
 //configuration settings
 
 mozdef = {
-  rootURL: "mozdef.private.scl3.mozilla.com",
+  rootURL: "localhost",
   port: "443",
-  rootAPI: "https://mozdef.private.scl3.mozilla.com:8444",
-  kibanaURL: "https://mozdef.private.scl3.mozilla.com:9443/app/kibana#",
+  rootAPI: "https://localhost:8444",
+  kibanaURL: "https://localhost:9443/app/kibana#",
   enableBlockIP: true,
   enableClientAccountCreation: false
 }
