aaf8ebb7b1b12b15ab96c2cd1d7cb053154e8d64
Brandon Myers <bmyers@mozilla.com>
2017-07-11 18:44:54 -0500

    Add extra tests to query string query model

--- tests/lib/query_models/test_query_string_match.py
+++ tests/lib/query_models/test_query_string_match.py
@@ -7,6 +7,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../lib"))
 from query_models import QueryStringMatch
 
 
+hostname_test_regex = 'hostname: /(.*\.)*(sub|bus)+(\..*)*\.abc(\..*)*\.company\.com/'
+
+
 class TestQueryStringMatchPositiveTestSuite(PositiveTestSuite):
     def query_tests(self):
         tests = {
@@ -18,6 +21,14 @@ class TestQueryStringMatchPositiveTestSuite(PositiveTestSuite):
                 {'summary': 'test'},
                 {'summary': 'conf'},
                 {'summary': 'test conf'},
+            ],
+
+            QueryStringMatch(hostname_test_regex): [
+                {'hostname': 'host1.sub.abc.company.com'},
+                {'hostname': 'host1.sub.test.abc.company.com'},
+                {'hostname': 'host1.sub.test.abc.domain.company.com'},
+                {'hostname': 'host1.sub.abc.domain.company.com'},
+                {'hostname': 'host2.bus.abc.domain.company.com'},
             ],
         }
         return tests
@@ -37,6 +48,15 @@ class TestQueryStringMatchNegativeTestSuite(NegativeTestSuite):
                 {'summary': 'testing'},
                 {'summary': 'configuration'},
                 {'summary': 'testing configuration'},
+            ],
+
+            QueryStringMatch(hostname_test_regex): [
+                {'hostname': 'host1.sub.abcd.company.com'},
+                {'hostname': 'host1.sub.dabc.company.com'},
+                {'hostname': 'host1.suba.abc.company.com'},
+                {'hostname': 'host1.asub.abc.company.com'},
+                {'hostname': 'host1.sub.dabc.domain.companyabc.com'},
+                {'hostname': 'host2.bus.abc.domain.abcompany.com'},
             ],
         }
         return tests
