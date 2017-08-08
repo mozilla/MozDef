0e5fc03e952cd3c9579cd90c36fed31f44cb05d6
Brandon Myers <bmyers@mozilla.com>
2017-08-08 11:29:15 -0500

    Update copyright date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MozDef'
-copyright = u'2017, Mozilla'
+copyright = u'2014, Mozilla'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
