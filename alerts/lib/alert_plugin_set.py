169de1e46a1f4fd5047c4a05cae7c90854ec4894
Brandon Myers <bmyers@mozilla.com>
2017-09-25 15:46:56 -0500

    Fixup alert plugin set

--- alerts/lib/alert_plugin_set.py
+++ alerts/lib/alert_plugin_set.py
@@ -8,9 +8,9 @@ from utilities.logger import logger
 
 class AlertPluginSet(PluginSet):
 
-    def send_message_to_plugin(self, plugin_class, message, metadata):
+    def send_message_to_plugin(self, plugin_class, message, metadata=None):
         if 'utctimestamp' in message and 'summary' in message:
             message_log_str = '{0} received message: ({1}) {2}'.format(plugin_class.__module__, message['utctimestamp'], message['summary'])
-            logger.info(message_log_str)
+            logger.error(message_log_str)
 
-        plugin_class.onMessage(message)
+        return plugin_class.onMessage(message), metadata
