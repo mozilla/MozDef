9d11f93e079b21219e111b47e85075e79af4b65f
Phrozyn <asmith@mozilla.com>
2017-10-04 11:21:20 -0500

    mergin upstream changes

---
+++ alerts/ssh_ioc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2015 Mozilla Corporation
+#
+# Contributors:
+# Aaron Meihm <ameihm@mozilla.com>
+
+from lib.alerttask import AlertTask
+from query_models import SearchQuery, TermMatch
+
+
+class AlertSSHIOC(AlertTask):
+    def main(self):
+        search_query = SearchQuery(minutes=30)
+
+        search_query.add_must([
+            TermMatch('_type', 'event'),
+            TermMatch('tags', 'mig-runner-sshioc'),
+        ])
+
+        self.filtersManual(search_query)
+
+        self.searchEventsSimple()
+        self.walkEvents()
+
+    # Set alert properties
+    def onEvent(self, event):
+        category = 'sshioc'
+        tags = ['sshioc']
+        severity = 'WARNING'
+
+        summary = 'SSH IOC match from runner plugin'
+        return self.createAlertDict(summary, category, tags, [event], severity)
