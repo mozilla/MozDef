9eef1d8dd8d276b7dd088a7e87e6b4af45dd9f21
Brandon Myers <bmyers@mozilla.com>
2017-10-05 12:12:26 -0500

    Add locality details to alert details

--- alerts/geomodel.py
+++ alerts/geomodel.py
@@ -49,4 +49,11 @@ class AlertGeomodel(AlertTask):
             severity = 'WARNING'
 
         summary = ev['summary']
-        return self.createAlertDict(summary, category, tags, [event], severity)
+        alert_dict = self.createAlertDict(summary, category, tags, [event], severity)
+
+        alert_dict['details'] = {
+            'locality_details': ev['details']['locality_details'],
+            'category': ev['details']['category'],
+        }
+
+        return alert_dict
