e8a4ecb4157fa889c1e0e495b087e406bcd2f8d7
Brandon Myers <bmyers@mozilla.com>
2017-11-09 13:15:53 -0600

    Modify generic dashboard plugin to specific geomodel plugin

--- alerts/plugins/dashboard_geomodel.py
+++ alerts/plugins/dashboard_geomodel.py
@@ -20,10 +20,11 @@ from utilities.logger import logger
 class message(object):
     def __init__(self):
         '''
-        sends certain messages to an SSO dashboard
+        sends geomodel alert to SSO dashboard
         '''
-        # Configuration loading
-        config_file_path = os.path.join(os.path.dirname(__file__), 'sso_dashboard.json')
+        self.alert_classname = 'AlertGeomodel'
+
+        config_file_path = os.path.join(os.path.dirname(__file__), 'dashboard_geomodel.json')
         json_obj = {}
         with open(config_file_path, "r") as fd:
             try:
@@ -32,13 +33,9 @@ class message(object):
                 logger.error("FAILED to open the configuration file" + str(config_file_path))
         self.config = json_obj
 
-        # Set the alert ids based on alert name
-        for name, alert in self.config['alerts'].iteritems():
-            alert['alert_code'] = b2a_hex(name)
-
         self.connect_db()
 
-        self.registration = '*'
+        self.registration = 'geomodel'
         self.priority = 1
 
     def connect_db(self):
@@ -56,47 +53,54 @@ class message(object):
         self.dynamodb.put_item(Item=alert_record)
 
     def onMessage(self, message):
-        for name, alert in self.config['alerts'].iteritems():
-            # we have a message that matches an alert we care about
-            if message['category'] == alert['category']:
-                if 'details' not in message:
-                    return message
+        if 'details' not in message:
+            return message
 
-                if 'principal' not in message['details']:
-                    return message
+        if 'principal' not in message['details']:
+            return message
 
-                if 'category' not in message['details']:
-                    return message
+        if 'category' not in message['details']:
+            return message
 
-                if message['details']['category'].lower() != 'newcountry':
-                    return message
+        if message['details']['category'].lower() != 'newcountry':
+            return message
 
-                full_email = message['details']['principal']
-                username = full_email.split('@')[0]
-                auth_full_username = self.config['auth_id_prefix'] + username
+        full_email = message['details']['principal']
+        username = full_email.split('@')[0]
+        auth_full_username = self.config['auth_id_prefix'] + username
 
-                if 'city' not in message['details']['locality_details']:
-                    return message
+        if 'city' not in message['details']['locality_details']:
+            return message
 
-                if 'country' not in message['details']['locality_details']:
-                    return message
+        if 'country' not in message['details']['locality_details']:
+            return message
 
-                city = message['details']['locality_details']['city']
-                country = message['details']['locality_details']['country']
-                summary = alert['summary'].format(city, country)
+        if 'source_ip' not in message['details']:
+            return message
 
-                alert_record = {
-                    'alert_id': b2a_hex(os.urandom(15)),
-                    'alert_code': alert['alert_code'],
-                    'user_id': auth_full_username,
-                    'risk': alert['risk'],
-                    'summary': summary,
-                    'description': alert['description'],
-                    'date': str(datetime.date.today()),
-                    'url': alert['url'],
-                    'url_title': alert['url_title'],
-                    'duplicate': alert['duplicate'],
-                }
-                self.write_db_entry(alert_record)
+        city = message['details']['locality_details']['city']
+        country = message['details']['locality_details']['country']
+        source_ip = message['details']['source_ip']
 
+        summary = 'Did you recently login from'
+        if city.lower() == 'unknown':
+            summary += ' {0}'.format(country)
+        else:
+            summary += ' {0}, {1}'.format(city, country)
+        summary += ' ({0})?'.format(source_ip)
+
+        alert_record = {
+            'alert_id': b2a_hex(os.urandom(15)),
+            'alert_code': b2a_hex(self.alert_classname),
+            'user_id': auth_full_username,
+            'risk': self.config['risk'],
+            'summary': summary,
+            'description': self.config['description'],
+            'date': str(datetime.date.today()),
+            'url': self.config['url'],
+            'url_title': self.config['url_title'],
+            'duplicate': self.config['duplicate'],
+            'alert_obj': message,
+        }
+        self.write_db_entry(alert_record)
         return message
