ea57d3353050adc79b951f334087b36004e934b7
Brandon Myers <bmyers@mozilla.com>
2017-11-13 22:24:14 -0600

    Update formatting for return message in mq plugin

--- tests/mq/plugins/test_filterlog.py
+++ tests/mq/plugins/test_filterlog.py
@@ -23,12 +23,15 @@ class TestFilterlog():
         metadata = {}
         metadata['doc_type'] = 'event'
 
-        expected_return_message = {'details': {'action': 'block',
-             'anchor': '',
-             'destinationipaddress': '21.143.56.109',
-             'direction': 'in',
-             'interface': 'igb0',
-             'ip': {'ecn': '',
+        expected_return_message = {
+            'details': {
+                'action': 'block',
+                'anchor': '',
+                'destinationipaddress': '21.143.56.109',
+                'direction': 'in',
+                'interface': 'igb0',
+                'ip': {
+                    'ecn': '',
                     'flags': 'DF',
                     'id': '60624',
                     'length': '92',
@@ -37,17 +40,22 @@ class TestFilterlog():
                     'protocol_text': 'udp',
                     'tos': '0x0',
                     'ttl': '6',
-                    'version': 4},
-             'ip_version': '4',
-             'reason': 'match',
-             'rule_number': '9',
-             'sourceipaddress': '175.41.7.2',
-             'sub_rule_number': '',
-             'trackor': '1000000103',
-             'udp': {'data_length': '72',
-                     'destination_port': '33443',
-                     'source_port': '57434'}},
- 'summary': '9,,,1000000103,igb0,match,block,in,4,0x0,,6,60624,0,DF,17,udp,92,175.41.7.2,21.143.56.109,57434,33443,72'}
+                    'version': 4
+                },
+                'ip_version': '4',
+                'reason': 'match',
+                'rule_number': '9',
+                'sourceipaddress': '175.41.7.2',
+                'sub_rule_number': '',
+                'trackor': '1000000103',
+                'udp': {
+                    'data_length': '72',
+                    'destination_port': '33443',
+                    'source_port': '57434'
+                }
+            },
+            'summary': '9,,,1000000103,igb0,match,block,in,4,0x0,,6,60624,0,DF,17,udp,92,175.41.7.2,21.143.56.109,57434,33443,72'
+        }
 
         (retmessage, retmeta) = self.plugin.onMessage(self.msg, metadata)
         assert retmessage == expected_return_message
