b006036528aa7c082d9be45c0dfaab1a563329c4
Brandon Myers <bmyers@mozilla.com>
2017-11-28 12:53:31 -0600

    Uppercase cloudtrail verb by default

--- tests/mq/test_esworker_cloudtrail.py
+++ tests/mq/test_esworker_cloudtrail.py
@@ -132,3 +132,16 @@ class TestKeyMapping():
         }
 
         assert result['details'] == expected_details
+
+    def test_cloudtrail_dict_eventname_lowercase(self):
+        cloudtrail_dict = {
+            'eventName': 'listDomains',
+        }
+        result = self.key_mapping(cloudtrail_dict)
+        expected_details = {
+            'eventReadOnly': True,
+            'eventVerb': 'List',
+            'eventname': 'ListDomains'
+        }
+        assert result['details'] == expected_details
+
