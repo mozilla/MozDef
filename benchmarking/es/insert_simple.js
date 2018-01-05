f2b0b78e6a007da484a1496cdc4a1b4e415e8cf4
Brandon Myers <bmyers@mozilla.com>
2018-01-05 12:31:54 -0600

    Remove leftover javascript comment line

--- benchmarking/es/insert_simple.js
+++ benchmarking/es/insert_simple.js
@@ -2,7 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // Copyright (c) 2014 Mozilla Corporation
-//
 
 // Usage: node ./insert_simple.js <processes> <totalInserts> <host1> [host2] [host3] [...]
 
