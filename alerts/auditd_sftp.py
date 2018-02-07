53fa503e04654c5366376854793a81e46a2f4262
Phrozyn <asmith@mozilla.com>
2018-02-07 17:46:07 -0600

    Corrected typo in auditd_sftp.py

--- alerts/auditd_sftp.py
+++ alerts/auditd_sftp.py
@@ -1,4 +1,4 @@
-5   #!/usr/bin/env python
+#!/usr/bin/env python
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
