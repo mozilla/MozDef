e4c5a64830a937d21613723dad4b15688ee8776e
Jeff Bryner <jeff@jeffbryner.com>
2018-05-15 08:34:06 -0700

    ipblocklist js to return the collection

---
+++ meteor/app/client/ipBlocklistTable.js
@@ -0,0 +1,19 @@
+ /*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+Copyright (c) 2014 Mozilla Corporation
+*/
+
+if (Meteor.isClient) {
+
+    //return all items
+    Template.ipblocklist.helpers({
+        ipblock: function () {
+            return ipblocklist.find({},{
+                                   sort: {dateExpiring: -1}
+                                });
+        }
+    });
+
+}
