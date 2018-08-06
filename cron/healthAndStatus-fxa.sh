62231136c9ee4510f795fed779e43923ac85e8ee
Brandon Myers <bmyers@mozilla.com>
2018-08-06 11:33:10 -0500

    Remove fxa health and status cron script

--- cron/healthAndStatus-fxa.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# Copyright (c) 2014 Mozilla Corporation
-
-source  /opt/mozdef/envs/mozdef/bin/activate
-/opt/mozdef/envs/mozdef/cron/healthAndStatus.py -c /opt/mozdef/envs/mozdef/cron/healthAndStatus.fxa.conf
