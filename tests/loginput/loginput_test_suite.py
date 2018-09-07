7d2affb28f4d18c955ef3f4459d2b73608b0eca2
Brandon Myers <bmyers@mozilla.com>
2018-09-07 14:42:35 -0500

    Fix rest and loginput test config locations

--- tests/loginput/loginput_test_suite.py
+++ tests/loginput/loginput_test_suite.py
@@ -10,16 +10,13 @@ from utilities.dot_dict import DotDict
 import mock
 from configlib import OptionParser
 
-sample_config = DotDict()
-sample_config.configfile = os.path.join(os.path.dirname(__file__), '../../loginput/index.conf')
-OptionParser.parse_args = mock.Mock(return_value=(sample_config, {}))
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
-
 
 class LoginputTestSuite(HTTPTestSuite):
 
     def setup(self):
+        sample_config = DotDict()
+        sample_config.configfile = os.path.join(os.path.dirname(__file__), '../../loginput/index.conf')
+        OptionParser.parse_args = mock.Mock(return_value=(sample_config, {}))
         from loginput import index
         self.application = index.application
         super(LoginputTestSuite, self).setup()
