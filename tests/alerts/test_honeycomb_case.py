426554bf1878f7e6953531d0ca9febd28fc452c5
Jonathan Claudius <jclaudius@mozilla.com>
2018-09-20 09:40:08 -0400

    Add unit-test examples for honeycomb alert

--- tests/alerts/test_honeycomb_case.py
+++ tests/alerts/test_honeycomb_case.py
@@ -20,17 +20,40 @@ class TestAlertHoneycomb(AlertTestSuite):
             "category": "syslog",
             "details": {
                 "processname": "Honeycomb"
-            }
+            },
+            "hostname": "foo.bar.com",
+            "summary": ('id="1a6c32ec-e900-464b-b517-da8845a9e735"'
+                        ' status="2" timestamp="2018-09-20 13:10:3'
+                        '7.889056" event_type="simple_http" event_'
+                        'description="HTTP Server Interaction" req'
+                        'uest="GET /" originating_ip="1.2.3.4'
+                        '" originating_port="60104" decoy_os="Linu'
+                        'x" __weakref__="None"')
         }
     }
+
+    # Generating another event to simulate multiple hostile source IPs
+    default_event2 = default_event.copy()
+    default_event2['_source']['summary'] = ('id="1a6c32ec-e900-464b-b517-da8845a9e735"'
+                                            ' status="2" timestamp="2018-09-20 13:10:3'
+                                            '7.889056" event_type="simple_http" event_'
+                                            'description="HTTP Server Interaction" req'
+                                            'uest="GET /" originating_ip="1.2.3.5'
+                                            '" originating_port="60104" decoy_os="Linu'
+                                            'x" __weakref__="None"')
 
     # This alert is the expected result from running this task
     default_alert = {
         "category": "honeypot",
         "tags": ['honeypot', 'honeycomb'],
         "severity": "WARNING",
-        "summary": 'Honeypot activity on 10.10.10.10 from IP(s): 1.2.3.4',
+        "summary": 'Honeypot activity on foo.bar.com from IP(s): 1.2.3.4',
     }
+
+    # This alert is the expected result from running this task
+    default_multi_ip_alert = default_alert.copy()
+    default_multi_ip_alert[
+        'summary'] = 'Honeypot activity on foo.bar.com from IP(s): 1.2.3.4,1.2.3.5'
 
     test_cases = []
 
@@ -39,6 +62,17 @@ class TestAlertHoneycomb(AlertTestSuite):
             description="Positive test with default events and default alert expected",
             events=AlertTestSuite.create_events(default_event, 10),
             expected_alert=default_alert
+        )
+    )
+
+    events1 = AlertTestSuite.create_events(default_event, 5)
+    events2 = AlertTestSuite.create_events(default_event2, 5)
+    events = events1 + events2
+    test_cases.append(
+        PositiveAlertTestCase(
+            description="Positive test with events with multiple IPs and default alert with a multi-IP summary",
+            events=events,
+            expected_alert=default_multi_ip_alert
         )
     )
 
@@ -54,18 +88,30 @@ class TestAlertHoneycomb(AlertTestSuite):
 
     events = AlertTestSuite.create_events(default_event, 10)
     for event in events:
-        event['_source']['tags'] = 'bad tag example'
+        event['_source']['category'] = 'bad category example'
     test_cases.append(
         NegativeAlertTestCase(
-            description="Negative test case with events with incorrect tags",
+            description="Negative test case with events with incorrect category",
             events=events,
         )
     )
 
     events = AlertTestSuite.create_events(default_event, 10)
     for event in events:
-        event['_source']['utctimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 241})
-        event['_source']['receivedtimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 241})
+        event['_source']['details']['processname'] = 'bad processname example'
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with events with incorrect processname",
+            events=events,
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 10)
+    for event in events:
+        event['_source']['utctimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({
+            'minutes': 241})
+        event['_source']['receivedtimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({
+            'minutes': 241})
     test_cases.append(
         NegativeAlertTestCase(
             description="Negative test case with old timestamp",
