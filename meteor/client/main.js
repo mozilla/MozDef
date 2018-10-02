b01c219d844f503e9047da2a7aea28168c6a2bbf
Jeff Bryner <jeff@jeffbryner.com>
2018-10-01 17:00:24 -0700

    move everything around to match new dir scheme

---
+++ meteor/client/main.js
@@ -0,0 +1,43 @@
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo';
+import { Session } from 'meteor/session';
+import { _ } from 'meteor/underscore';
+import { Blaze } from 'meteor/blaze';
+import '/imports/collections.js';
+import './router.js';
+import './mozdef.js';
+import '/imports/settings.js';
+import '/client/about.html';
+import '/client/alertdetails.html';
+import '/client/alertssummary.html';
+import '/client/attackerdetails.html';
+import '/client/attackers.html';
+import '/client/blockFQDN.html';
+import '/client/blockIP.html';
+import '/client/eventdetails.html';
+import '/client/fqdnBlocklistTable.html';
+import '/client/globe.html';
+import '/client/incidentAdd.html';
+import '/client/incidentEdit.html';
+import '/client/incidentsveris.html';
+import '/client/incidentTable.html';
+import '/client/investigationAdd.html';
+import '/client/investigationEdit.html';
+import '/client/investigationTable.html';
+import '/client/ipBlocklistTable.html';
+import '/client/ipdshield.html';
+import '/client/ipintel.html';
+import '/client/ipwhois.html';
+
+if (Meteor.isClient) {
+    //client side collections:
+    alertsCount = new Mongo.Collection("alerts-count");
+    //client-side subscriptions to low volume collections
+    Meteor.subscribe("mozdefsettings");
+    Meteor.subscribe("veris");
+    Meteor.subscribe("kibanadashboards");
+    Meteor.subscribe("userActivity");
+
+};
---
+++ meteor/server/main.js
@@ -0,0 +1,358 @@
+import { Meteor } from 'meteor/meteor';
+import '/imports/settings.js';
+import '/imports/collections.js'
+import './methods.js';
+import './mozdef.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+
+
+if (Meteor.isServer) {
+    //Publishing setups
+    Meteor.publish("mozdefsettings",function(){
+        return mozdefsettings.find();
+    });
+
+    Meteor.publish("alerts-summary", function (searchregex,timeperiod,recordlimit) {
+        //tail the last 100 records by default
+
+        //default parameters
+        timeperiod = typeof timeperiod !== 'undefined' ? timeperiod: 'tail';
+        searchregex = typeof searchregex !== 'undefined' ? searchregex: '';
+        recordlimit = ['number'].indexOf(typeof(recordlimit)) ? 100:recordlimit;
+        //sanity check the record limit
+        if ( recordlimit >10000 || recordlimit < 1){
+            recordlimit = 100;
+        }
+
+        if ( timeperiod ==='tail' || timeperiod == 'none' ){
+            return alerts.find(
+                {summary: {$regex:searchregex}},
+                {fields:{
+                        _id:1,
+                        esmetadata:1,
+                        utctimestamp:1,
+                        utcepoch:1,
+                        summary:1,
+                        severity:1,
+                        category:1,
+                        acknowledged:1,
+                        acknowledgedby:1,
+                        url:1
+                        },
+                   sort: {utcepoch: -1},
+                   limit:recordlimit}
+            );
+        } else {
+            //determine the utcepoch range
+            beginningtime=moment().utc();
+            //expect timeperiod like '1 days'
+            timevalue=Number(timeperiod.split(" ")[0]);
+            timeunits=timeperiod.split(" ")[1];
+            beginningtime.subtract(timevalue,timeunits);
+            return alerts.find(
+                {summary: {$regex:searchregex},
+                utcepoch: {$gte: beginningtime.unix()}},
+                {fields:{
+                        _id:1,
+                        esmetadata:1,
+                        utctimestamp:1,
+                        utcepoch:1,
+                        summary:1,
+                        severity:1,
+                        category:1,
+                        acknowledged:1
+                        },
+                   sort: {utcepoch: -1},
+                   limit:recordlimit}
+            );
+        }
+    });
+
+    Meteor.publish("alerts-details",function(alertid,includeEvents){
+       //return alerts.find({'esmetadata.id': alertid});
+       //alert ids can be either mongo or elastic search IDs
+       //look for both to publish to the collection.
+        //default parameters
+        includeEvents = typeof includeEvents !== 'undefined' ? includeEvents: true;
+        if ( includeEvents ){
+            return alerts.find({
+                                $or:[
+                                        {'esmetadata.id': alertid},
+                                        {'_id': alertid},
+                                    ]
+             });
+        }else{
+            return alerts.find({
+                                $or:[
+                                        {'esmetadata.id': alertid},
+                                        {'_id': alertid},
+                                    ]
+                                },
+                                {fields:{events:0},
+             });
+        }
+    });
+
+    Meteor.publish("alerts-count", function () {
+      var self = this;
+      var count = 0;
+      var initializing = true;
+      var recordID=Meteor.uuid();
+
+      //get a count by watching for only 1 new entry sorted in reverse date order.
+      //use that hook to return a find().count rather than iterating the entire result set over and over
+      var handle = alerts.find({}, {sort: {utcepoch: -1},limit:1}).observeChanges({
+        added: function (newDoc,oldDoc) {
+            count=alerts.find().count();
+            if (!initializing) {
+                self.changed("alerts-count", recordID,{count: count});
+                //console.log('added alerts count to' + count);
+            }
+        },
+
+        changed: function (newDoc,oldDoc) {
+            count=alerts.find().count();
+            if (!initializing) {
+                self.changed("alerts-count", recordID,{count: count});
+                //console.log('changed alerts count to' + count);
+            }
+        },
+
+        removed: function (newDoc,oldDoc) {
+            count=alerts.find().count();
+            if (!initializing) {
+                self.changed("alerts-count", recordID,{count: count});
+                //console.log('changed alerts count to' + count);
+            }
+        }
+      });
+      initializing = false;
+      self.added("alerts-count", recordID,{count: count});
+      //console.log('count is ready: ' + count);
+      self.ready();
+
+      // Stop observing the cursor when client unsubs.
+      // Stopping a subscription automatically takes
+      // care of sending the client any removed messages.
+      self.onStop(function () {
+        //console.log('stopped publishing alerts count.')
+        handle.stop();
+      });
+    });
+
+    //publish the last X event/alerts
+    //using document index instead of date
+//    Meteor.publish("attacker-details",function(attackerid){
+//       return attackers.find({'_id': attackerid},
+//                             {fields: {
+//                                events:{$slice: 20,
+//                                        $sort: { documentindex: -1 }},
+//                                alerts:{$slice: -10}
+//                             }}
+//                             );
+//    });
+
+     Meteor.publish("attacker-details",function(attackerid){
+       return attackers.find({'_id': attackerid},
+                             {fields: {
+                                events:{$slice: -20},
+                                alerts:{$slice: -10}
+                             },
+                             sort: { 'events.documentsource.utctimestamp': -1 },
+                             reactive:false
+                             }
+                             );
+    });
+
+
+
+    Meteor.publish("attackers-summary", function () {
+    //limit to the last 100 records by default
+    //to ease the sync transfer to dc.js/crossfilter
+    return attackers.find({},
+                    {fields:{
+                            events:0,
+                            alerts:0,
+                            },
+                       sort: {lastseentimestamp: -1},
+                       limit:100});
+    });
+
+    Meteor.publish("attackers-summary-landmass", function () {
+        //limit to the last 100 records by default
+        //to ease the sync transfer to dc.js/crossfilter
+        var inModifier = { $in: ["broxss", "brotunnel", "brosqli"]};
+        return attackers.find({"events.documentsource.category": inModifier},
+                              {sort: {lastseentimestamp: -1},
+                               limit: 100});
+    });
+
+    Meteor.publish("investigations-summary", function () {
+        return investigations.find({},
+                              {fields: {
+                                        _id:1,
+                                        summary:1,
+                                        phase:1,
+                                        dateOpened:1,
+                                        dateClosed:1,
+                                        creator:1
+                                },
+                              sort: {dateOpened: -1},
+                              limit:100});
+    });
+
+    Meteor.publish("investigation-details",function(investigationid){
+       return investigations.find({'_id': investigationid});
+    });
+
+    Meteor.publish("incidents-summary", function () {
+        return incidents.find({},
+                              {fields: {
+                                        _id:1,
+                                        summary:1,
+                                        phase:1,
+                                        dateOpened:1,
+                                        dateClosed:1,
+                                        creator:1
+                                },
+                              sort: {dateOpened: -1},
+                              limit:100});
+    });
+
+    Meteor.publish("incident-details",function(incidentid){
+       return incidents.find({'_id': incidentid});
+    });
+
+    Meteor.publish("veris", function () {
+        return veris.find({}, {limit:0});
+    });
+
+    Meteor.publish("healthfrontend", function () {
+        return healthfrontend.find({}, {limit:0});
+    });
+
+    Meteor.publish("healthescluster", function () {
+        return healthescluster.find({}, {limit:0});
+    });
+
+    Meteor.publish("healthesnodes", function () {
+        return healthesnodes.find({}, {limit:0});
+    });
+
+    Meteor.publish("healtheshotthreads", function () {
+        return healtheshotthreads.find({}, {limit:0});
+    });
+
+    Meteor.publish("kibanadashboards", function () {
+        return kibanadashboards.find({},{sort:{name:1}, limit:30});
+    });
+
+    Meteor.publish("userActivity", function () {
+        return userActivity.find({},{sort:{userID:1}, limit:100});
+    });
+
+    Meteor.publish("ipblocklist", function () {
+        return ipblocklist.find({},{limit:0});
+    })
+
+    Meteor.publish("fqdnblocklist", function () {
+        return fqdnblocklist.find({},{limit:0});
+    })
+
+   //access rules from clients
+   //barebones to allow you to specify rules
+
+   incidents.allow({
+      insert: function (userId, doc) {
+        // the user must be logged in
+        return (userId);
+      },
+      update: function (userId, doc, fields, modifier) {
+        // the user must be logged in
+        return (userId);
+      },
+      remove: function (userId, doc) {
+        // can only remove one's own indicents
+        return doc.creator === Meteor.user().profile.email;
+      },
+      fetch: ['creator']
+    });
+
+    attackers.allow({
+      update: function (userId, doc, fields, modifier) {
+        // the user must be logged in
+        return (userId);
+      }
+    });
+
+    alerts.allow({
+      update: function (userId, doc, fields, modifier) {
+        // the user must be logged in
+        return (userId);
+      }
+    });
+
+    investigations.allow({
+      insert: function (userId, doc) {
+        // the user must be logged in
+        return (userId);
+      },
+      update: function (userId, doc, fields, modifier) {
+        // the user must be logged in
+        return (userId);
+      },
+      remove: function (userId, doc) {
+        // can only remove one's own items
+        return doc.creator === Meteor.user().profile.email;
+      },
+      fetch: ['creator']
+    });
+
+    userActivity.allow({
+      insert: function (userId, doc) {
+        // the user must be logged in
+        return (userId);
+      },
+      remove: function (userId, doc) {
+        // can only remove one's own items
+        return doc.userId === Meteor.user().profile.email;
+      },
+    });
+
+    ipblocklist.allow({
+      insert: function (userId, doc) {
+        // the user must be logged in
+        return (userId);
+      },
+      update: function (userId, doc, fields, modifier) {
+        // the user must be logged in
+        return (userId);
+      },
+      remove: function (userId, doc) {
+        // the user must be logged in
+        return (userId);
+      },
+      fetch: ['creator']
+    });
+
+    fqdnblocklist.allow({
+      insert: function (userId, doc) {
+        // the user must be logged in
+        return (userId);
+      },
+      update: function (userId, doc, fields, modifier) {
+        // the user must be logged in
+        return (userId);
+      },
+      remove: function (userId, doc) {
+        // the user must be logged in
+        return (userId);
+      },
+      fetch: ['creator']
+    });
+
+};
+
+});
