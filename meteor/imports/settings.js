1e60686bf1227989b6a6883fb78d14d69cae84d5
Jeff Bryner <jeff@jeffbryner.com>
2018-10-19 16:52:35 -0700

    indents

--- meteor/imports/settings.js
+++ meteor/imports/settings.js
@@ -8,16 +8,17 @@ Copyright (c) 2017 Mozilla Corporation
 //configuration settings
 if (Meteor.isServer) {
     mozdef = {
-    rootURL: process.env.OPTIONS_METEOR_ROOTURL || "http://localhost",
-    port: process.env.OPTIONS_METEOR_PORT || "80",
-    rootAPI: process.env.OPTIONS_METEOR_ROOTAPI || "http://rest:8081",
-    kibanaURL: process.env.OPTIONS_METEOR_KIBANAURL || "http://localhost:9090/app/kibana#",
-    enableBlockIP: process.env.OPTIONS_METEOR_ENABLEBLOCKIP || true,
-    enableClientAccountCreation: process.env.OPTIONS_METEOR_ENABLECLIENTACCOUNTCREATION || true,
-    authenticationType: process.env.OPTIONS_METEOR_AUTHENTICATIONTYPE || "meteor-password"
+        rootURL: process.env.OPTIONS_METEOR_ROOTURL || "http://localhost",
+        port: process.env.OPTIONS_METEOR_PORT || "80",
+        rootAPI: process.env.OPTIONS_METEOR_ROOTAPI || "http://rest:8081",
+        kibanaURL: process.env.OPTIONS_METEOR_KIBANAURL || "http://localhost:9090/app/kibana#",
+        enableBlockIP: process.env.OPTIONS_METEOR_ENABLEBLOCKIP || true,
+        enableClientAccountCreation: process.env.OPTIONS_METEOR_ENABLECLIENTACCOUNTCREATION || true,
+        authenticationType: process.env.OPTIONS_METEOR_AUTHENTICATIONTYPE || "meteor-password"
     }
 
     Meteor.settings.public=mozdef;
+
 }
 
 if (Meteor.isClient) {
