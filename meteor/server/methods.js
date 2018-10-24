f490118ed4f3ee5f9d73f0f1089e84b1f38ec9d3
A Smith <Phrozyn@users.noreply.github.com>
2018-10-23 20:48:10 -0500

    Update methods.js

--- meteor/server/methods.js
+++ meteor/server/methods.js
@@ -16,7 +16,7 @@ if (Meteor.isServer) {
         'loadKibanaDashboards': loadKibanaDashboards,
         'blockip': blockIP,
         'blockfqdn': blockFQDN,
-        'watchitem': watchitem,
+        'watchitem': watchItem,
         'ipwhois': ipwhois,
         'ipdshield': ipdshield,
         'ipintel': ipintel,
