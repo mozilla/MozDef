22d669e4179acbf91bd9f8809da431a8997cdfe1
Brandon Myers <bmyers@mozilla.com>
2018-10-26 14:55:32 -0500

    Fix imports missing in papertrail worker

--- mq/esworker_papertrail.py
+++ mq/esworker_papertrail.py
@@ -22,14 +22,13 @@ import requests
 
 from mozdef_util.elasticsearch_client import ElasticsearchClient, ElasticsearchBadServer, ElasticsearchInvalidIndex, ElasticsearchException
 
-from utilities.toUTC import toUTC
-from utilities.to_unicode import toUnicode
-from utilities.remove_at import removeAt
-from utilities.is_cef import isCEF
+from mozdef_util.utilities.toUTC import toUTC
+from mozdef_util.utilities.to_unicode import toUnicode
+from mozdef_util.utilities.remove_at import removeAt
+from mozdef_util.utilities.is_cef import isCEF
+from mozdef_util.utilities.logger import logger, initLogger
 
 from lib.plugins import sendEventToPlugins, registerPlugins
-
-from utilities.logger import logger, initLogger
 
 
 # running under uwsgi?
