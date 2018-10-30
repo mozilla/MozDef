944dde209b8fc06d67e4218233764b12bfe23645
Brandon Myers <bmyers@mozilla.com>
2018-10-30 17:52:53 -0500

    Resolve E301 expected 1 line got 0

--- cron/duo_logpull.py
+++ cron/duo_logpull.py
@@ -15,8 +15,10 @@ except ImportError:
     class UTC(tzinfo):
         def utcoffset(self, dt):
             return timedelta(0)
+
         def tzname(self, dt):
             return "UTC"
+
         def dst(self, dt):
             return timedelta(0)
     utc = UTC()
