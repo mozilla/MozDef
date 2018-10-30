67e2e29fb23e53c73655fe8df9779f7e8cc69796
Cag <30646014+caggle@users.noreply.github.com>
2018-10-30 15:39:45 +1100

    Create an alert on attempts to use known exfil domains
    For example, pastebin.com

---
+++ alerts/proxy_drop_exfil_domains.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2014 Mozilla Corporation
+
+
+from lib.alerttask import AlertTask
+from mozdef_util.query_models import QueryStringMatch, SearchQuery, TermMatch
+
+
+class AlertProxyDropExfilDomain(AlertTask):
+    def main(self):
+        self.parse_config('proxy_drop_exfil_domains.conf', ['exfil_domains'])
+
+        search_query = SearchQuery(minutes=20)
+
+        search_query.add_must([
+            TermMatch('category', 'squid'),
+            TermMatch('tags', 'squid'),
+            TermMatch('details.proxyaction', 'TCP_DENIED/-')
+        ])
+
+        # Only notify on certain domains listed in the config
+        domain_regex = "/({0}).*/".format(
+            self.config.exfil_daomins.replace(',', '|'))
+        search_query.add_must([
+            QueryStringMatch('details.destination: {}'.format(domain_regex))
+        ])
+
+        self.filtersManual(search_query)
+
+        # Search aggregations on field 'hostname', keep X samples of
+        # events at most
+        self.searchEventsAggregated('details.sourceipaddress', samplesLimit=10)
+        # alert when >= X matching events in an aggregation
+        # I think it makes sense to alert every time here
+        self.walkAggregations(threshold=1)
+
+    # Set alert properties
+    def onAggregation(self, aggreg):
+        # aggreg['count']: number of items in the aggregation, ex: number of failed login attempts
+        # aggreg['value']: value of the aggregation field, ex: toto@example.com
+        # aggreg['events']: list of events in the aggregation
+        category = 'squid'
+        tags = ['squid', 'proxy']
+        severity = 'WARNING'
+
+        dropped_domains = set()
+        for event in aggreg['allevents']:
+            dropped_domains.add(event['_source']['details']['destination'])
+
+        summary = 'Suspicious Proxy DROP event(s) detected from {0} to the following exfil domains: {1}'.format(
+            aggreg['value'],
+            ",".join(sorted(dropped_domains))
+        )
+
+        # Create the alert object based on these properties
+        return self.createAlertDict(summary, category, tags, aggreg['events'], severity)
