ce36bf575c2c2628b9d0b934a2b3c30e23fded24
Phrozyn <asmith@mozilla.com>
2018-11-06 11:48:11 -0600

    Fixing indentation error in eventStats.py

--- cron/eventStats.py
+++ cron/eventStats.py
@@ -85,10 +85,10 @@ def main():
                 logger.debug("The index is not created. Terminating eventStats.py cron job.")
                 exit(1)
             sleepcycles += 1
-            if es.index_exists(index):
-                # post to elastic search servers directly without going through
-                # message queues in case there is an availability issue
-                es.save_event(index=index, body=json.dumps(stats), doc_type='mozdefstats')
+        if es.index_exists(index):
+            # post to elastic search servers directly without going through
+            # message queues in case there is an availability issue
+            es.save_event(index=index, body=json.dumps(stats), doc_type='mozdefstats')
 
     except Exception as e:
         logger.error("Exception %r when gathering statistics " % e)
