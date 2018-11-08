bfbe86c79c86353172859f4e0e3a1010765c4381
Michal Purzynski <michal@mozilla.com>
2018-11-07 17:31:18 -0800

    The standard alert version of the Address_Scan

---
+++ alerts/nsm_scan_address.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2018 Mozilla Corporation
+
+from lib.alerttask import AlertTask
+from mozdef_util.query_models import SearchQuery, TermMatch, QueryStringMatch, PhraseMatch, ExistsMatch
+
+
+class NSMScanAddress(AlertTask):
+    def __init__(self):
+        AlertTask.__init__(self)
+        self._config = self.parse_json_alert_config('nsm_scan_address.json')
+
+    def main(self):
+        search_query = SearchQuery(minutes=1)
+        search_query.add_must([
+            TermMatch('category', 'bro'),
+            TermMatch('details.source', 'notice'),
+            PhraseMatch('details.note', 'Scan::Address_Scan'),
+            QueryStringMatch('details.sourceipaddress: {}'.format(self._config['sourcemustmatch']))
+        ])
+        search_query.add_must_not([
+            QueryStringMatch('details.sourceipaddress: {}'.format(self._config['sourcemustnotmatch']))
+        ])
+
+        self.filtersManual(search_query)
+        self.searchEventsAggregated('details.sourceipaddress', samplesLimit=10)
+        self.walkAggregations(threshold=1)
+
+    def onAggregation(self, aggreg):
+        category = 'nsm'
+        severity = 'NOTICE'
+        tags = ['nsm', "bro", 'addressscan']
+
+        indicators = 'unknown'
+        x = aggreg['events'][0]['_source']
+        if 'details' in x:
+            if 'indicators' in x['details']:
+                indicators = x['details']['sourceipaddress']
+
+        summary = 'Address scan from {}'.format(indicators)
+
+        return self.createAlertDict(summary, category, tags, aggreg['events'], severity)
---
+++ tests/alerts/test_nsm_scan_address.py
@@ -0,0 +1,134 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2017 Mozilla Corporation
+
+from positive_alert_test_case import PositiveAlertTestCase
+from negative_alert_test_case import NegativeAlertTestCase
+
+from alert_test_suite import AlertTestSuite
+
+
+class TestNSMScanAddress(AlertTestSuite):
+    alert_filename = "nsm_scan_address"
+
+    # This event is the default positive event that will cause the
+    # alert to trigger
+    default_event = {
+        "_source": {
+            "category": "bro",
+            "summary": "Scan::Address_Scan source 10.252.25.90 destination unknown port unknown",
+            "hostname": "your.friendly.nsm.sensor",
+            "tags": ["bro"],
+            "details": {
+                "sourceipaddress": "10.99.88.77",
+                "indicators": "10.99.88.77",
+                "source": "notice",
+                "note": "Scan::Address_Scan",
+            }
+        }
+    }
+
+    # This alert is the expected result from running this task
+    default_alert = {
+        "category": "nsm",
+        "severity": "NOTICE",
+        "summary": "Address scan from 10.99.88.77",
+        "tags": ['nsm', 'bro', 'addressscan'],
+        "notify_mozdefbot": False,
+    }
+
+    test_cases = []
+
+    test_cases.append(
+        PositiveAlertTestCase(
+            description="Positive test with default event and default alert expected",
+            events=AlertTestSuite.create_events(default_event, 5),
+            expected_alert=default_alert
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['utctimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda(date_timedelta={'minutes': 1})
+        event['_source']['receivedtimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda(date_timedelta={'minutes': 1})
+    test_cases.append(
+        PositiveAlertTestCase(
+            description="Positive test with events a minute earlier",
+            events=events,
+            expected_alert=default_alert
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['category'] = 'syslog'
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with a different category",
+            events=events,
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['details']['source'] = 'intel'
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with a different details.source",
+            events=events,
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['details']['note'] = 'Scan::Random_Scan'
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with a different scan type (note)",
+            events=events,
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['details']['note'] = 'Scan::Port_Scan'
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with a different scan type (note)",
+            events=events,
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['details']['sourceipaddress'] = '10.54.65.234'
+        event['_source']['details']['indicators'] = '1.2.3.4'
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with an excluded IP address",
+            events=events,
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['details']['sourceipaddress'] = '1.2.3.4'
+        event['_source']['details']['indicators'] = '1.2.3.4'
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with an excluded subnet",
+            events=events,
+        )
+    )
+
+    events = AlertTestSuite.create_events(default_event, 5)
+    for event in events:
+        event['_source']['utctimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 15})
+        event['_source']['receivedtimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 15})
+    test_cases.append(
+        NegativeAlertTestCase(
+            description="Negative test case with old timestamp",
+            events=events,
+        )
+    )
