661c0a04ec2de38f7d3c7ca93e09c7570b31c4b5
Brandon Myers <bmyers@mozilla.com>
2018-12-17 15:14:32 -0600

    Add user, path and downgrade severity for audit write alert

--- alerts/write_audit.py
+++ alerts/write_audit.py
@@ -13,7 +13,7 @@ from mozdef_util.query_models import SearchQuery, TermMatch, PhraseMatch
 
 class WriteAudit(AlertTask):
     def main(self):
-        self.parse_config('write_audit.conf', ['skipprocess'])
+        self.parse_config('write_audit.conf', ['skipprocess', 'expectedusers'])
         search_query = SearchQuery(minutes=15)
 
         search_query.add_must([
@@ -33,7 +33,22 @@ class WriteAudit(AlertTask):
         severity = 'WARNING'
         tags = ['audit']
 
-        summary = ('{0} Filesystem write(s) to an auditd path by {1}'.format(aggreg['count'], aggreg['value'], ))
+        users = set()
+        paths = set()
+        for event in aggreg['events']:
+            users.add(event['_source']['details']['user'])
+            paths.add(event['_source']['summary'].split(' ')[1])
+
+        summary = '{0} Filesystem write(s) to an auditd path ({1}) by {2} ({3})'.format(
+            aggreg['count'],
+            ', '.join(paths),
+            ', '.join(users),
+            aggreg['value']
+        )
+
+        if aggreg['value'] in self.config.expectedusers.split(' '):
+            severity = 'NOTICE'
+
         hostnames = self.mostCommon(aggreg['allevents'], '_source.hostname')
         # did they modify more than one host?
         # or just modify an existing configuration more than once?
