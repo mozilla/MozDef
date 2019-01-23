75649e7f72e33b6d820706cf3adbf8823753d189
Michal Purzynski <michal@mozilla.com>
2019-01-23 12:22:09 -0800

    More tests

--- tests/mq/plugins/test_squidFixup.py
+++ tests/mq/plugins/test_squidFixup.py
@@ -204,7 +204,99 @@ class TestSquidFixup(object):
     #def test_access_ssl_allow_log(self):
     #event['MESSAGE'] = "1547953357.127   1690 192.168.97.135 58306 185.53.177.31 443 TCP_TUNNEL - 86 4700 CONNECT test.com:443 - test.com -"
 
-    def test_access_http_allow_log(self):
+    def test_access_http_allow_custom_port_log(self):
+        event = {
+            "tags":"squid",
+            "source":"access",
+            "customendpoint":" ",
+            "category":"proxy",
+            "TAGS":".source.access_src",
+            "SOURCEIP":"127.0.0.1",
+            "SOURCE":"access_src",
+            "PRIORITY":"notice",
+            "MESSAGE":"1548043032.187    524 192.168.97.135 58322 185.53.177.31 444 TCP_MISS 200 152 339 GET http://test.com:444/ http test.com text/html",
+            "HOST_FROM":"localhost",
+            "HOST":"localhost",
+            "FILE_NAME":"/etc/syslog-ng/access.log.local",
+            "FACILITY":"user",
+            "DATE":"Jan 21 03:57:42"
+        }
+        result, metadata = self.plugin.onMessage(event, self.metadata)
+        self.verify_defaults(result)
+        self.verify_metadata(metadata)
+        #assert toUTC(result['timestamp']).isoformat() == result['timestamp']
+        #assert toUTC(result['utctimestamp']).isoformat() == result['utctimestamp']
+        assert result['details']['sourceipaddress'] == '192.168.97.135'
+        assert result['details']['destinationipaddress'] == '185.53.177.31'
+        assert result['details']['sourceport'] == 58322
+        assert result['details']['destinationport'] == 444
+        assert result['details']['duration'] == 0.524
+        assert result['details']['requestsize'] == 152
+        assert result['details']['responsesize'] == 339
+        assert result['details']['proxyaction'] == 'TCP_MISS'
+        assert result['details']['status'] == '200'
+        assert result['details']['method'] == 'GET'
+        assert result['details']['destination'] == 'http://test.com:444/'
+        assert result['details']['proto'] == 'http'
+        assert result['details']['host'] == 'test.com'
+        assert result['details']['mimetype'] == 'text/html'
+        assert 'TAGS' not in result
+        assert 'SOURCEIP' not in result
+        assert 'PRIORITY' not in result
+        assert 'MESSAGE' not in result
+        assert 'HOST_FROM' not in result
+        assert 'HOST' not in result
+        assert 'FILE_NAME' not in result
+        assert 'FACILITY' not in result
+        assert 'DATE' not in result
+
+    def test_access_http_allow_without_uri_log(self):
+        event = {
+            "tags":"squid",
+            "source":"access",
+            "customendpoint":" ",
+            "category":"proxy",
+            "TAGS":".source.access_src",
+            "SOURCEIP":"127.0.0.1",
+            "SOURCE":"access_src",
+            "PRIORITY":"notice",
+            "MESSAGE":"1548043032.187    412 192.168.97.135 58322 185.53.177.31 80 TCP_MISS 403 158 453 GET http://test.com/ http test.com text/html",
+            "HOST_FROM":"localhost",
+            "HOST":"localhost",
+            "FILE_NAME":"/etc/syslog-ng/access.log.local",
+            "FACILITY":"user",
+            "DATE":"Jan 21 03:57:42"
+        }
+        result, metadata = self.plugin.onMessage(event, self.metadata)
+        self.verify_defaults(result)
+        self.verify_metadata(metadata)
+        #assert toUTC(result['timestamp']).isoformat() == result['timestamp']
+        #assert toUTC(result['utctimestamp']).isoformat() == result['utctimestamp']
+        assert result['details']['sourceipaddress'] == '192.168.97.135'
+        assert result['details']['destinationipaddress'] == '185.53.177.31'
+        assert result['details']['sourceport'] == 58322
+        assert result['details']['destinationport'] == 80
+        assert result['details']['duration'] == 0.412
+        assert result['details']['requestsize'] == 158
+        assert result['details']['responsesize'] == 453
+        assert result['details']['proxyaction'] == 'TCP_MISS'
+        assert result['details']['status'] == '403'
+        assert result['details']['method'] == 'GET'
+        assert result['details']['destination'] == 'http://test.com/'
+        assert result['details']['proto'] == 'http'
+        assert result['details']['host'] == 'test.com'
+        assert result['details']['mimetype'] == 'text/html'
+        assert 'TAGS' not in result
+        assert 'SOURCEIP' not in result
+        assert 'PRIORITY' not in result
+        assert 'MESSAGE' not in result
+        assert 'HOST_FROM' not in result
+        assert 'HOST' not in result
+        assert 'FILE_NAME' not in result
+        assert 'FACILITY' not in result
+        assert 'DATE' not in result
+
+    def test_access_http_allow_with_uri_log(self):
         event = {
             "tags":"squid",
             "source":"access",
@@ -250,7 +342,191 @@ class TestSquidFixup(object):
         assert 'FACILITY' not in result
         assert 'DATE' not in result
 
+    def test_access_http_allow_with_uri_custom_port_log(self):
+        event = {
+            "tags":"squid",
+            "source":"access",
+            "customendpoint":" ",
+            "category":"proxy",
+            "TAGS":".source.access_src",
+            "SOURCEIP":"127.0.0.1",
+            "SOURCE":"access_src",
+            "PRIORITY":"notice",
+            "MESSAGE":"1548043032.187    412 192.168.97.135 58322 185.53.177.31 444 TCP_MISS 403 158 453 GET http://test.com:444/3something.bin http test.com text/html",
+            "HOST_FROM":"localhost",
+            "HOST":"localhost",
+            "FILE_NAME":"/etc/syslog-ng/access.log.local",
+            "FACILITY":"user",
+            "DATE":"Jan 21 03:57:42"
+        }
+        result, metadata = self.plugin.onMessage(event, self.metadata)
+        self.verify_defaults(result)
+        self.verify_metadata(metadata)
+        #assert toUTC(result['timestamp']).isoformat() == result['timestamp']
+        #assert toUTC(result['utctimestamp']).isoformat() == result['utctimestamp']
+        assert result['details']['sourceipaddress'] == '192.168.97.135'
+        assert result['details']['destinationipaddress'] == '185.53.177.31'
+        assert result['details']['sourceport'] == 58322
+        assert result['details']['destinationport'] == 444
+        assert result['details']['duration'] == 0.412
+        assert result['details']['requestsize'] == 158
+        assert result['details']['responsesize'] == 453
+        assert result['details']['proxyaction'] == 'TCP_MISS'
+        assert result['details']['status'] == '403'
+        assert result['details']['method'] == 'GET'
+        assert result['details']['destination'] == 'http://test.com:444/3something.bin'
+        assert result['details']['proto'] == 'http'
+        assert result['details']['host'] == 'test.com'
+        assert result['details']['mimetype'] == 'text/html'
+        assert 'TAGS' not in result
+        assert 'SOURCEIP' not in result
+        assert 'PRIORITY' not in result
+        assert 'MESSAGE' not in result
+        assert 'HOST_FROM' not in result
+        assert 'HOST' not in result
+        assert 'FILE_NAME' not in result
+        assert 'FACILITY' not in result
+        assert 'DATE' not in result
+
+    def test_access_ssl_allow(self):
+        event = {
+            "tags":"squid",
+            "source":"access",
+            "customendpoint":" ",
+            "category":"proxy",
+            "TAGS":".source.access_src",
+            "SOURCEIP":"127.0.0.1",
+            "SOURCE":"access_src",
+            "PRIORITY":"notice",
+            "MESSAGE":"1548043024.368   1082 192.168.97.135 58318 185.53.177.31 443 TCP_TUNNEL - 86 4700 CONNECT test.com:443 - test.com -",
+            "HOST_FROM":"localhost",
+            "HOST":"localhost",
+            "FILE_NAME":"/etc/syslog-ng/access.log.local",
+            "FACILITY":"user",
+            "DATE":"Jan 21 03:57:42"
+        }
+        result, metadata = self.plugin.onMessage(event, self.metadata)
+        self.verify_defaults(result)
+        self.verify_metadata(metadata)
+        #assert toUTC(result['timestamp']).isoformat() == result['timestamp']
+        #assert toUTC(result['utctimestamp']).isoformat() == result['utctimestamp']
+        assert result['details']['sourceipaddress'] == '192.168.97.135'
+        assert result['details']['destinationipaddress'] == '185.53.177.31'
+        assert result['details']['sourceport'] == 58318
+        assert result['details']['destinationport'] == 443
+        assert result['details']['duration'] == 1.082
+        assert result['details']['requestsize'] == 86
+        assert result['details']['responsesize'] == 4700
+        assert result['details']['proxyaction'] == 'TCP_TUNNEL'
+        assert result['details']['status'] == '-'
+        assert result['details']['method'] == 'CONNECT'
+        assert result['details']['destination'] == 'test.com:443'
+        assert result['details']['proto'] == 'ssl'
+        assert result['details']['host'] == 'test.com'
+        assert result['details']['mimetype'] == '-'
+        assert 'TAGS' not in result
+        assert 'SOURCEIP' not in result
+        assert 'PRIORITY' not in result
+        assert 'MESSAGE' not in result
+        assert 'HOST_FROM' not in result
+        assert 'HOST' not in result
+        assert 'FILE_NAME' not in result
+        assert 'FACILITY' not in result
+        assert 'DATE' not in result
+
+    def test_access_ssl_deny(self):
+        event = {
+            "tags":"squid",
+            "source":"access",
+            "customendpoint":" ",
+            "category":"proxy",
+            "TAGS":".source.access_src",
+            "SOURCEIP":"127.0.0.1",
+            "SOURCE":"access_src",
+            "PRIORITY":"notice",
+            "MESSAGE":"1548043048.377      0 192.168.97.135 58332 - - TCP_DENIED - 86 3892 CONNECT test.com:444 - - text/html",
+            "HOST_FROM":"localhost",
+            "HOST":"localhost",
+            "FILE_NAME":"/etc/syslog-ng/access.log.local",
+            "FACILITY":"user",
+            "DATE":"Jan 21 03:57:42"
+        }
+        result, metadata = self.plugin.onMessage(event, self.metadata)
+        self.verify_defaults(result)
+        self.verify_metadata(metadata)
+        #assert toUTC(result['timestamp']).isoformat() == result['timestamp']
+        #assert toUTC(result['utctimestamp']).isoformat() == result['utctimestamp']
+        assert result['details']['sourceipaddress'] == '192.168.97.135'
+        assert result['details']['destinationipaddress'] == '0.0.0.0'
+        assert result['details']['sourceport'] == 58332
+        assert result['details']['destinationport'] == 444
+        assert result['details']['duration'] == 0.0
+        assert result['details']['requestsize'] == 86
+        assert result['details']['responsesize'] == 3892
+        assert result['details']['proxyaction'] == 'TCP_DENIED'
+        assert result['details']['status'] == '-'
+        assert result['details']['method'] == 'CONNECT'
+        assert result['details']['destination'] == 'test.com:444'
+        assert result['details']['proto'] == 'ssl'
+        assert result['details']['host'] == 'test.com'
+        assert result['details']['mimetype'] == 'text/html'
+        assert 'TAGS' not in result
+        assert 'SOURCEIP' not in result
+        assert 'PRIORITY' not in result
+        assert 'MESSAGE' not in result
+        assert 'HOST_FROM' not in result
+        assert 'HOST' not in result
+        assert 'FILE_NAME' not in result
+        assert 'FACILITY' not in result
+        assert 'DATE' not in result
+
     def test_access_http_deny_log(self):
+        event = {
+            "tags":"squid",
+            "source":"access",
+            "customendpoint":" ",
+            "category":"proxy",
+            "TAGS":".source.access_src",
+            "SOURCEIP":"127.0.0.1",
+            "SOURCE":"access_src",
+            "PRIORITY":"notice",
+            "MESSAGE":"1548043040.983      0 192.168.97.135 58328 - - TCP_DENIED - 152 3992 GET http://test.com:444/ http - text/html",
+            "HOST_FROM":"localhost",
+            "HOST":"localhost",
+            "FILE_NAME":"/etc/syslog-ng/access.log.local",
+            "FACILITY":"user",
+            "DATE":"Jan 21 03:57:42"
+        }
+        result, metadata = self.plugin.onMessage(event, self.metadata)
+        self.verify_defaults(result)
+        self.verify_metadata(metadata)
+        #assert toUTC(result['timestamp']).isoformat() == result['timestamp']
+        #assert toUTC(result['utctimestamp']).isoformat() == result['utctimestamp']
+        assert result['details']['sourceipaddress'] == '192.168.97.135'
+        assert result['details']['destinationipaddress'] == '0.0.0.0'
+        assert result['details']['sourceport'] == 58328
+        assert result['details']['destinationport'] == 444
+        assert result['details']['duration'] == 0.0
+        assert result['details']['requestsize'] == 152
+        assert result['details']['responsesize'] == 3992
+        assert result['details']['proxyaction'] == 'TCP_DENIED'
+        assert result['details']['status'] == '-'
+        assert result['details']['method'] == 'GET'
+        assert result['details']['destination'] == 'http://test.com:444/'
+        assert result['details']['proto'] == 'http'
+        assert result['details']['host'] == 'test.com'
+        assert result['details']['mimetype'] == 'text/html'
+        assert 'TAGS' not in result
+        assert 'SOURCEIP' not in result
+        assert 'PRIORITY' not in result
+        assert 'MESSAGE' not in result
+        assert 'HOST_FROM' not in result
+        assert 'HOST' not in result
+        assert 'FILE_NAME' not in result
+        assert 'FACILITY' not in result
+        assert 'DATE' not in result
+
+    def test_access_http_deny_with_uri_log(self):
         event = {
             "tags":"squid",
             "source":"access",
