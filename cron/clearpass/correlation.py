c793161ce8a23ed9592e904b4c0977910ce97e8d
Michal Purzynski <michal@mozilla.com>
2019-01-29 20:35:56 -0800

    pep8

--- cron/clearpass/correlation.py
+++ cron/clearpass/correlation.py
@@ -29,7 +29,6 @@ class RingBuffer:
 
 
 class UsernameNetResolve:
-
     def __init__(self):
         self.logger = logging.getLogger(sys.argv[0])
         self.initParser()
@@ -38,7 +37,7 @@ class UsernameNetResolve:
         self.initYap()
         self.macassignments = self.readOUIFile()
         self.esClient = ElasticsearchClient(self.options.esreadurl)
-        self.logger.debug('started')
+        self.logger.debug("started")
         self.seenRing = RingBuffer(100)
 
     def loggerTimeStamp(self, record, datefmt=None):
@@ -47,170 +46,193 @@ class UsernameNetResolve:
     def initLogger(self):
         self.logger.level = logging.DEBUG
         formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
         formatter.formatTime = self.loggerTimeStamp
-        if self.options.output == 'syslog':
+        if self.options.output == "syslog":
             self.logger.addHandler(
-                SysLogHandler(address=(self.options.sysloghostname,
-                                       self.options.syslogport)))
+                SysLogHandler(
+                    address=(self.options.sysloghostname, self.options.syslogport)
+                )
+            )
         else:
             sh = logging.StreamHandler(sys.stderr)
             sh.setFormatter(formatter)
             self.logger.addHandler(sh)
 
     def initConfig(self):
-        self.options.output= getConfig('output', 'stdout', self.options.configfile)
-        self.options.esreadurl= getConfig('esreadurl', 'localhost:9200', self.options.configfile)
-        self.options.eswriteurl= getConfig('eswriteurl', 'localhost:9200', self.options.configfile)
-        self.options.cpmap= getConfig('cpmap', '<empty>', self.options.configfile)
-        self.options.dhcpexclude= getConfig('dhcpexclude', '<empty>', self.options.configfile)
+        self.options.output = getConfig("output", "stdout", self.options.configfile)
+        self.options.esreadurl = getConfig(
+            "esreadurl", "localhost:9200", self.options.configfile
+        )
+        self.options.eswriteurl = getConfig(
+            "eswriteurl", "localhost:9200", self.options.configfile
+        )
+        self.options.cpmap = getConfig("cpmap", "<empty>", self.options.configfile)
+        self.options.dhcpexclude = getConfig(
+            "dhcpexclude", "<empty>", self.options.configfile
+        )
 
     def initParser(self):
         parser = OptionParser()
         parser.add_option(
             "-c",
-            dest='configfile',
-            default=sys.argv[0].replace('.py', '.conf'),
-            help="configuration file to use")
+            dest="configfile",
+            default=sys.argv[0].replace(".py", ".conf"),
+            help="configuration file to use",
+        )
         (self.options, args) = parser.parse_args()
 
     def initYap(self):
-        with open(os.path.join(os.path.dirname(__file__), self.options.cpmap), 'r') as f:
+        with open(
+            os.path.join(os.path.dirname(__file__), self.options.cpmap), "r"
+        ) as f:
             map = f.read()
 
         self.yap = load(map)
-        del(map)
+        del (map)
 
     def readOUIFile(self):
-        '''
+        """
         Expects the OUI file from IEEE:
         wget http://www.ieee.org/netstorage/standards/oui.txt
         Reads the (hex) line and extracts the hex prefix and the vendor name
         to store as part of the intelligence record about what device the user
         was seen using.
-        '''
-        ouifilename='oui.txt'
-        # ugly, change to with, catch exception, print exception, test that
+        """
+        ouifilename = "oui.txt"
         with open(ouifilename) as ouifile:
-            macassignments={}
+            macassignments = {}
             for i in ouifile.readlines()[0::]:
-                i=i.strip()
-                if '(hex)' in i:
-                    fields=i.split('\t')
-                    macprefix=fields[0][0:8].replace('-',':').lower()
-                    entity=fields[2]
-                    macassignments[macprefix]=entity
-        self.logger.debug('The MAC OUI database loaded - %s entries', len(macassignments))
+                i = i.strip()
+                if "(hex)" in i:
+                    fields = i.split("\t")
+                    macprefix = fields[0][0:8].replace("-", ":").lower()
+                    entity = fields[2]
+                    macassignments[macprefix] = entity
+        self.logger.debug(
+            "The MAC OUI database loaded - %s entries", len(macassignments)
+        )
         return macassignments
 
     def find_mac_by_ip(self):
-        ip = compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'+
-                     '(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
-        mac = compile('([a-fA-F0-9]{2}[:|\-]?){6}')
+        ip = compile(
+            "(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}"
+            + "(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))"
+        )
+        mac = compile("([a-fA-F0-9]{2}[:|\-]?){6}")
 
         search_query = SearchQuery(hours=8)
-        search_query.add_must([
-            TermMatch('category', 'syslog'),
-            TermMatch('processname', 'dhcpd'),
-            QueryStringMatch('summary: DHCPOFFER')
-        ])
-        for de in self.options.dhcpexclude.split(' '):
-            search_query.add_must_not([
-                TermMatch('hostname', de)
-            ])
-        events = search_query.execute(self.esClient, indices=['events-weekly'])
+        search_query.add_must(
+            [
+                TermMatch("category", "syslog"),
+                TermMatch("processname", "dhcpd"),
+                QueryStringMatch("summary: DHCPOFFER"),
+            ]
+        )
+        for de in self.options.dhcpexclude.split(" "):
+            search_query.add_must_not([TermMatch("hostname", de)])
+        events = search_query.execute(self.esClient, indices=["events-weekly"])
 
-        for event in events['hits']:
+        for event in events["hits"]:
             try:
-                match_ip = ip.search(event['_source']['summary']).group()
+                match_ip = ip.search(event["_source"]["summary"]).group()
             except:
                 pass
             try:
-                match_mac = mac.search(event['_source']['summary']).group()
+                match_mac = mac.search(event["_source"]["summary"]).group()
             except:
                 pass
-            self.logger.debug('%s <- %s', match_ip, match_mac)
+            self.logger.debug("%s <- %s", match_ip, match_mac)
             if match_ip + match_mac in self.seenRing.get():
                 return
             else:
                 self.seenRing.append(match_ip + match_mac)
             self.find_username_by_mac(match_mac, match_ip)
 
-        self.logger.debug('Found %s DHCP events',len(events['hits']))
+        self.logger.debug("Found %s DHCP events", len(events["hits"]))
 
     def find_username_by_mac(self, match_mac, match_ip):
         newmessage = {}
-        newmessage['details'] = {}
+        newmessage["details"] = {}
 
         macobj = EUI(match_mac)
         try:
             oui = macobj.oui
-            newmessage['details']['hwvendor'] = oui.registration().org
+            newmessage["details"]["hwvendor"] = oui.registration().org
         except NotRegisteredError as e:
-            self.logger.debug('netaddr failed as usual - %s', e)
+            self.logger.debug("netaddr failed as usual - %s", e)
             pass
         macobj.dialect = mac_bare
         mac_bare_str = str(macobj)
 
         if match_mac[0:8].lower() in self.macassignments:
-            newmessage['details']['hwvendor'] = self.macassignments[match_mac[0:8].lower()]
-            self.logger.debug('found vendor in the out.txt')
+            newmessage["details"]["hwvendor"] = self.macassignments[
+                match_mac[0:8].lower()
+            ]
+            self.logger.debug("found vendor in the out.txt")
 
         search_query = SearchQuery(hours=8)
-        search_query.add_must([
-            TermMatch('category', 'syslog'),
-            TermMatch('facility', 'local1'),
-            QueryStringMatch('summary: ' + mac_bare_str)
-        ])
-        events = search_query.execute(self.esClient, indices=['events-weekly'])
-        for event in events['hits']:
-            message = event['_source']['summary']
-        self.logger.debug('Found %s Radius events',len(events['hits']))
+        search_query.add_must(
+            [
+                TermMatch("category", "syslog"),
+                TermMatch("facility", "local1"),
+                QueryStringMatch("summary: " + mac_bare_str),
+            ]
+        )
+        events = search_query.execute(self.esClient, indices=["events-weekly"])
+        for event in events["hits"]:
+            message = event["_source"]["summary"]
+        self.logger.debug("Found %s Radius events", len(events["hits"]))
 
-        if len(events['hits']) == 0:
+        if len(events["hits"]) == 0:
             return
 
-        message_broken = message.split(',')
+        message_broken = message.split(",")
 
         message_dict = {}
         for f in message_broken:
-            if '=' not in f:
+            if "=" not in f:
                 continue
-            k, v = f.split('=')
+            k, v = f.split("=")
             message_dict[k] = v
-        del(message_broken)
+        del (message_broken)
 
-        for key in self.yap['session']:
-            mappedvalue = jmespath.search(self.yap['session'][key], message_dict)
+        for key in self.yap["session"]:
+            mappedvalue = jmespath.search(self.yap["session"][key], message_dict)
             if mappedvalue is not None:
-                newmessage['details'][key] = mappedvalue
-        del(message_dict)
+                newmessage["details"][key] = mappedvalue
+        del (message_dict)
 
-        newmessage['details']['macaddress'] = str(EUI(newmessage['details']['macaddress']))
+        newmessage["details"]["macaddress"] = str(
+            EUI(newmessage["details"]["macaddress"])
+        )
 
-        if newmessage['details']['result'] == 'REJECT':
-            newmessage['details']['success'] = False
-        if newmessage['details']['result'] == 'ACCEPT':
-            newmessage['details']['success'] = True
+        if newmessage["details"]["result"] == "REJECT":
+            newmessage["details"]["success"] = False
+        if newmessage["details"]["result"] == "ACCEPT":
+            newmessage["details"]["success"] = True
 
-        newmessage['details']['eventipaddress'] = match_ip
+        newmessage["details"]["eventipaddress"] = match_ip
 
-        if len(events['hits']) > 0:
+        if len(events["hits"]) > 0:
             self.createevent(newmessage)
 
         return
 
     def createevent(self, newmessage):
-        self.logger.debug('%s', newmessage)
+        self.logger.debug("%s", newmessage)
 
         mozmsg = mozdef.MozDefEvent(self.options.eswriteurl)
-        mozmsg.tags = ['authentication', 'clearpass', 'dhcp']
-        mozmsg.set_category('authentication')
+        mozmsg.tags = ["authentication", "clearpass", "dhcp"]
+        mozmsg.set_category("authentication")
 
-        mozmsg.timestamp = toUTC(newmessage['details']['radiustimestamp']).isoformat()
+        mozmsg.timestamp = toUTC(newmessage["details"]["radiustimestamp"]).isoformat()
 
-        mozmsg.details = newmessage['details']
-        mozmsg.summary = '%s <- %s'.format(newmessage['details']['username'], newmessage['details']['eventipaddress'])
+        mozmsg.details = newmessage["details"]
+        mozmsg.summary = "%s <- %s".format(
+            newmessage["details"]["username"], newmessage["details"]["eventipaddress"]
+        )
 
         mozmsg.send()
 
@@ -220,5 +242,5 @@ def main():
     iptouser.find_mac_by_ip()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
