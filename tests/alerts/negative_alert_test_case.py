5361f58aba4631f00beb654b1b73271590a92cd9
Jon Moroney <darakian@gmail.com>
2019-02-04 11:06:05 -0800

    Assert expected_alert is None. A negative case taking an expected alert is counter intuitive at best.

--- tests/alerts/negative_alert_test_case.py
+++ tests/alerts/negative_alert_test_case.py
@@ -2,4 +2,5 @@ from alert_test_case import AlertTestCase
 
 
 class NegativeAlertTestCase(AlertTestCase):
+    assert self.expected_alert is None, "NegativeAlertTestCase is used to test cases where an alert does not occur. Therefore expected_alert should be None."
     expected_test_result = False
