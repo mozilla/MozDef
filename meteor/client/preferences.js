0f89781228ef06dd8c17832892815073db84e16c
Jeff Bryner <jeff@jeffbryner.com>
2019-02-22 14:59:22 -0800

    auto-set a theme when picked

--- meteor/client/preferences.js
+++ meteor/client/preferences.js
@@ -5,47 +5,51 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 Copyright (c) 2014 Mozilla Corporation
 */
 
-if (Meteor.isClient) {
+if ( Meteor.isClient ) {
 
     //select an incident for editing
-    Template.preferences.events({
-        "click .something": function(e, t) {
+    Template.preferences.events( {
+        "change #theme": function( e, t ) {
+            savePreferences( e, t );
+            Meteor.setTimeout( function() {
+                location.reload( true );
+            }, 1000 );
         },
-        'input #preferences': _.debounce(function(e, t) {
-            savePreferences(e, t);
-        }, 500),
+        'input #preferences': _.debounce( function( e, t ) {
+            savePreferences( e, t );
+        }, 500 ),
 
-    });
+    } );
 
     Template.preferences.rendered = function() {
         // Deps.autorun(function () {
         //     Meteor.subscribe("preferences", Meteor.user().profile.email);
         // });
 
-        savePreferences = function(e, template) {
+        savePreferences = function( e, template ) {
             var preferencesobj = {
-                name: template.find("#name").value,
-                email: template.find("#email").value,
-                theme: template.find("#theme").value
+                name: template.find( "#name" ).value,
+                email: template.find( "#email" ).value,
+                theme: template.find( "#theme" ).value
             }
 
-            preferences.update(template.data._id,
+            preferences.update( template.data._id,
                 { $set: preferencesobj },
                 {},
-                function(error, nobj) {
-                    if (!error && nobj === 1) {
-                        $('#saveMessage').text('Changes Saved');
+                function( error, nobj ) {
+                    if ( !error && nobj === 1 ) {
+                        $( '#saveMessage' ).text( 'Changes Saved' );
                     } else {
-                        $('#saveMessage').text('Changes Not Saved');
-                        console.log(error, template);
+                        $( '#saveMessage' ).text( 'Changes Not Saved' );
+                        console.log( error, template );
                     }
                 }
             );
 
             //clear the info message after a bit
-            Meteor.setTimeout(function() {
-                $('#saveMessage').text('');
-            }, 3000);
+            Meteor.setTimeout( function() {
+                $( '#saveMessage' ).text( '' );
+            }, 3000 );
         }
     };
 
