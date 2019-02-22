7ca51bbb754e5b6b5161e69558745fbbe0a1fb5b
Michal Purzynski <michal@mozilla.com>
2019-02-21 18:03:10 -0800

    More updates to alerts on anomalies from proxy logs

--- alerts/proxy_drop_ip.py
+++ alerts/proxy_drop_ip.py
@@ -15,47 +15,46 @@ class AlertProxyDropIP(AlertTask):
     def main(self):
         search_query = SearchQuery(minutes=20)
 
-        search_query.add_must([
-            TermMatch('category', 'squid'),
-            TermMatch('tags', 'squid'),
-            TermMatch('details.proxyaction', 'TCP_DENIED/-')
-        ])
+        search_query.add_must(
+            [
+                TermMatch("category", "proxy"),
+                TermMatch("details.proxyaction", "TCP_DENIED"),
+            ]
+        )
 
         # Match on 1.1.1.1, http://1.1.1.1, or https://1.1.1.1
         # This will over-match on short 3-char domains like foo.bar.baz.com, but will get weeded out below
-        ip_regex = '/.*\..{1,3}\..{1,3}\..{1,3}(:.*|\/.*)/'
-        search_query.add_must([
-            QueryStringMatch('details.destination: {}'.format(ip_regex))
-        ])
+        ip_regex = "/.*\..{1,3}\..{1,3}\..{1,3}(:.*|\/.*)/"
+        search_query.add_must(
+            [QueryStringMatch("details.destination: {}".format(ip_regex))]
+        )
 
         self.filtersManual(search_query)
-        self.searchEventsAggregated('details.sourceipaddress', samplesLimit=10)
+        self.searchEventsAggregated("details.sourceipaddress", samplesLimit=10)
         self.walkAggregations(threshold=1)
 
     # Set alert properties
     def onAggregation(self, aggreg):
-        category = 'squid'
-        tags = ['squid', 'proxy']
-        severity = 'WARNING'
+        category = "squid"
+        tags = ["squid", "proxy"]
+        severity = "WARNING"
 
         # Lucene search has a slight potential for overmatches, so we'd double-check
         # with this pattern to ensure it's truely an IP before we add dest to our dropped list
-        pattern = r'^(http:\/\/|https:\/\/|)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+        pattern = r"^(http:\/\/|https:\/\/|)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
 
         dropped_destinations = set()
 
-        for event in aggreg['allevents']:
-            if re.search(pattern, event['_source']['details']['destination']):
-                dropped_destinations.add(
-                    event['_source']['details']['destination'])
+        for event in aggreg["allevents"]:
+            if re.search(pattern, event["_source"]["details"]["destination"]):
+                dropped_destinations.add(event["_source"]["details"]["destination"])
 
         # If it's all over-matches, don't throw the alert
         if len(dropped_destinations) == 0:
             return None
 
-        summary = 'Suspicious Proxy DROP event(s) detected from {0} to the following IP-based destination(s): {1}'.format(
-            aggreg['value'],
-            ",".join(sorted(dropped_destinations))
+        summary = "Suspicious Proxy DROP event(s) detected from {0} to the following IP-based destination(s): {1}".format(
+            aggreg["value"], ",".join(sorted(dropped_destinations))
         )
 
-        return self.createAlertDict(summary, category, tags, aggreg['events'], severity)
+        return self.createAlertDict(summary, category, tags, aggreg["events"], severity)
--- alerts/proxy_drop_non_standard_port.py
+++ alerts/proxy_drop_non_standard_port.py
@@ -12,47 +12,41 @@ from mozdef_util.query_models import QueryStringMatch, SearchQuery, TermMatch
 
 class AlertProxyDropNonStandardPort(AlertTask):
     def main(self):
-        self.parse_config(
-            'proxy_drop_non_standard_port.conf', ['excludedports'])
+        self.parse_config("proxy_drop_non_standard_port.conf", ["excludedports"])
 
         search_query = SearchQuery(minutes=20)
 
-        search_query.add_must([
-            TermMatch('category', 'squid'),
-            TermMatch('tags', 'squid'),
-            TermMatch('details.proxyaction', 'TCP_DENIED/-'),
-            TermMatch('details.tcpaction', 'CONNECT')
-        ])
-
-        # Only notify on certain ports from config
-        port_regex = "/.*:({0})/".format(
-            self.config.excludedports.replace(',', '|'))
-        search_query.add_must_not([
-            QueryStringMatch('details.destination: {}'.format(port_regex))
-        ])
+        search_query.add_must(
+            [
+                TermMatch("category", "proxy"),
+                TermMatch("details.proxyaction", "TCP_DENIED"),
+                TermMatch("details.method", "CONNECT"),
+            ]
+        )
+        for port in self.config.excludedports.split(","):
+            search_query.add_must_not([TermMatch("details.destinationport", port)])
 
         self.filtersManual(search_query)
 
         # Search aggregations on field 'hostname', keep X samples of
         # events at most
-        self.searchEventsAggregated('details.sourceipaddress', samplesLimit=10)
+        self.searchEventsAggregated("details.sourceipaddress", samplesLimit=10)
         # alert when >= X matching events in an aggregation
         # I think it makes sense to alert every time here
         self.walkAggregations(threshold=1)
 
     def onAggregation(self, aggreg):
-        category = 'squid'
-        tags = ['squid', 'proxy']
-        severity = 'WARNING'
+        category = "squid"
+        tags = ["squid", "proxy"]
+        severity = "WARNING"
 
         destinations = set()
-        for event in aggreg['allevents']:
-            destinations.add(event['_source']['details']['destination'])
+        for event in aggreg["allevents"]:
+            destinations.add(event["_source"]["details"]["destination"])
 
-        summary = 'Suspicious Proxy DROP event(s) detected from {0} to the following non-std port destination(s): {1}'.format(
-            aggreg['value'],
-            ",".join(sorted(destinations))
+        summary = "Suspicious Proxy DROP event(s) detected from {0} to the following non-std port destination(s): {1}".format(
+            aggreg["value"], ",".join(sorted(destinations))
         )
 
         # Create the alert object based on these properties
-        return self.createAlertDict(summary, category, tags, aggreg['events'], severity)
+        return self.createAlertDict(summary, category, tags, aggreg["events"], severity)
