6ec70758feefdfa255253c813b8b92f89300a646
Michal Purzynski <michal@mozilla.com>
2019-02-25 17:29:00 -0800

    More of pepism

--- alerts/proxy_drop_non_standard_port.py
+++ alerts/proxy_drop_non_standard_port.py
@@ -7,7 +7,7 @@
 
 
 from lib.alerttask import AlertTask
-from mozdef_util.query_models import QueryStringMatch, SearchQuery, TermMatch
+from mozdef_util.query_models import SearchQuery, TermMatch
 
 
 class AlertProxyDropNonStandardPort(AlertTask):
