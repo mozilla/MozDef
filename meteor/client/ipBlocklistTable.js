f7f3408a1168add99d46db5890d0c2878b68392e
Jeff Bryner <jeff@jeffbryner.com>
2019-04-18 09:46:56 -0700

    formatting ipblocklist js

--- meteor/client/ipBlocklistTable.js
+++ meteor/client/ipBlocklistTable.js
@@ -1,39 +1,39 @@
- /*
+/*
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 Copyright (c) 2014 Mozilla Corporation
 */
 
-if (Meteor.isClient) {
+if ( Meteor.isClient ) {
 
     //return all items
-    Template.ipblocklist.helpers({
-        ipblock: function () {
-            return ipblocklist.find({},{
-                                   sort: {dateExpiring: -1}
-                                });
+    Template.ipblocklist.helpers( {
+        ipblock: function() {
+            return ipblocklist.find( {}, {
+                sort: { dateExpiring: -1 }
+            } );
         }
-    });
+    } );
 
     //select an incident for editing
-    Template.ipblocklist.events({
-        "click .ipblockadd": function(e,t){
+    Template.ipblocklist.events( {
+        "click .ipblockadd": function( e, t ) {
             //clear any leftover ip session val
-            Session.set('blockIPipaddress','');
-            $('#modalBlockIPWindow').modal();
+            Session.set( 'blockIPipaddress', '' );
+            $( '#modalBlockIPWindow' ).modal();
         },
 
-        "click .ipblockdelete": function(e,t){
-            ipblocklist.remove(this._id);
-            Session.set('displayMessage','Deleted ipblock for & ' + this.address);
+        "click .ipblockdelete": function( e, t ) {
+            ipblocklist.remove( this._id );
+            Session.set( 'displayMessage', 'Deleted ipblock for & ' + this.address );
         }
-    });
+    } );
 
-    Template.ipblocklist.rendered = function(){
-        Deps.autorun(function() {
-            Meteor.subscribe("ipblocklist");
-        });
+    Template.ipblocklist.rendered = function() {
+        Deps.autorun( function() {
+            Meteor.subscribe( "ipblocklist" );
+        } );
 
     };
 
