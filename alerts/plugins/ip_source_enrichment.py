d3ba77a8865a2e67ad596e921d9f28041dc86296
Emma Rose <emrose@mozilla.com>
2019-04-29 20:42:37 -0400

    Refactoring code to match the preferred interface

--- alerts/plugins/ip_source_enrichment.py
+++ alerts/plugins/ip_source_enrichment.py
@@ -3,8 +3,39 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # Copyright (c) 2014 Mozilla Corporation
 
+import netaddr
+
 
 CONFIG_FILE = './ip_source_enrichment.json.conf'
+
+
+def _isIPv4(ip):
+    try:
+        return netaddr.valid_ipv4(ip)
+    except:
+        return False
+
+
+def _isIPv6(ip):
+    try:
+        return netaddr.valid_ipv6(ip)
+    except:
+        return False
+
+
+def _find_ip_addresses(string):
+    '''List all of the IPv4 and IPv6 addresses found in a string.'''
+
+    return []
+
+
+def enrich(self, alert, known_ips):
+    '''Add information to alerts containing IP addresses that describes
+    the source location of the IP address if it can be determined based
+    on a configured mapping.
+    '''
+    
+    return alert
 
 
 def _load_config(file_path):
@@ -16,78 +47,37 @@ def _load_config(file_path):
     return {}
 
 
-class _RegexMatchRule(object):
-    '''Private
+class message(object):
+    '''Alert plugin interface that handles messages (alerts).
+    This plugin will look for IP addresses in any of the values of an
+    alert dictionary.  For each IP address found, it will append some
+    text to the summary of the alert to provide more information
+    about where the IP originates from if it is recognized.
 
-    A rule defining a mapping from a regular expression matching a
-    set of IP addresses to a format string accepting one parameter.
+    The expected format of the configuration file,
+    `ip_source_enrichment.json.conf`, is as follows:
 
-    Produced by EnrichIPs given a plugin configuration.
-    '''
-
-    def __init__(self, regex, fmt):
-        '''Initialize the rule with a regular expression and
-        format string.
-        '''
-
-        self._ip_regex = regex
-        self._format_string = fmt
-
-
-    def matches(self, input_str):
-        '''Produces a list of strings each produced by formatting
-        the configured format string with any IPs found in the
-        provided input.
-        '''
-
-        return []
-
-
-class _CIDRMatchRule(object):
-    '''Private
-
-    A rule defining a mapping from a CIDR address to a format string
-    that accepts one parameter.
-
-    Produced by EnrichIPs given a plugin configuration.
-    '''
-   
-    def __init__(self, cidr_str, fmt):
-        '''Initialize the rule with an IPv4 or IPv6 CIDR address
-        string and format string.
-        '''
-
-        self._cidr = cidr_str
-        self._format_string = fmt
-
-        
-    def matches(self, input_str):
-        '''Produces a list of strings each produced by formatting
-        the configured format string with any IPs found in the
-        provided input.
-        '''
-
-        return []
-
-
-class EnrichIPs(object):
-    '''Add information to alerts containing IP addresses that describes
-    the source location of the IP address if it can be determined based
-    on a configured mapping.
+    ```json
+    {
+      "known": [
+        {
+          "ipVersion": 4,
+          "range": "1.2.3.4/8",
+          "format": "IPv4 {1} is known"
+        },
+        {
+          "ipVersion": 6,
+          "range": "1a2b:3c4d:123::/48",
+          "format": "IPv6 {1} is known"
+        }
+      ]
+    }
+    ```
     '''
 
     def __init__(self):
-        '''Initialize the plugin from a configuration in preparation to
-        match alerts.
-        '''
-
-        self.configuration = _load_config(CONFIG_FILE)
+        self._config = _load_config(CONFIG_FILE)
 
 
-    def enrich(self, alert):
-        '''Enrich alerts containing IP addresses with information about
-        the location from which those IPs originate.
-        Returns a modified alert.
-        '''
-
-        return alert
+    def onMessage(self, message):
+        return enrich(message, self._configj)
