b34a206865d3cd6cca681d3286ad160d644eced7
Andrew Krug <akrug@mozilla.com>
2019-05-08 10:57:01 -0700

    first pass at support for SQS transport for alerttask

--- alerts/celeryconfig.py
+++ alerts/celeryconfig.py
@@ -6,26 +6,20 @@ from logging.config import dictConfig
 # Alert files to include
 alerts_include = []
 for alert in ALERTS.keys():
-    alerts_include.append('.'.join((alert).split('.')[:-1]))
+    alerts_include.append(".".join((alert).split(".")[:-1]))
 alerts_include = list(set(alerts_include))
 
-BROKER_URL = 'amqp://{0}:{1}@{2}:{3}//'.format(
-    RABBITMQ['mquser'],
-    RABBITMQ['mqpassword'],
-    RABBITMQ['mqserver'],
-    RABBITMQ['mqport']
+BROKER_URL = "amqp://{0}:{1}@{2}:{3}//".format(
+    RABBITMQ["mquser"], RABBITMQ["mqpassword"], RABBITMQ["mqserver"], RABBITMQ["mqport"]
 )
 CELERY_DISABLE_RATE_LIMITS = True
 CELERYD_CONCURRENCY = 1
 CELERY_IGNORE_RESULT = True
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_DEFAULT_QUEUE = 'celery-default'
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_DEFAULT_QUEUE = "celery-default"
 CELERY_QUEUES = {
-    'celery-default': {
-        "exchange": "celery-default",
-        "binding_key": "celery-default",
-    },
+    "celery-default": {"exchange": "celery-default", "binding_key": "celery-default"}
 }
 
 CELERYBEAT_SCHEDULE = {}
@@ -33,15 +27,15 @@ CELERYBEAT_SCHEDULE = {}
 # Register frequency of the tasks in the scheduler
 for alert in ALERTS.keys():
     CELERYBEAT_SCHEDULE[alert] = {
-        'task': alert,
-        'schedule': ALERTS[alert]['schedule'],
-        'options': {'queue': 'celery-default', "exchange": "celery-default"},
+        "task": alert,
+        "schedule": ALERTS[alert]["schedule"],
+        "options": {"queue": "celery-default", "exchange": "celery-default"},
     }
     # add optional parameters:
-    if 'args' in ALERTS[alert]:
-        CELERYBEAT_SCHEDULE[alert]['args']=ALERTS[alert]['args']
-    if 'kwargs' in ALERTS[alert]:
-        CELERYBEAT_SCHEDULE[alert]['kwargs']=ALERTS[alert]['kwargs']
+    if "args" in ALERTS[alert]:
+        CELERYBEAT_SCHEDULE[alert]["args"] = ALERTS[alert]["args"]
+    if "kwargs" in ALERTS[alert]:
+        CELERYBEAT_SCHEDULE[alert]["kwargs"] = ALERTS[alert]["kwargs"]
 
 # Load logging config
 dictConfig(LOGGING)
@@ -52,27 +46,26 @@ dictConfig(LOGGING)
 # app.conf.update(
 #     CELERY_TASK_RESULT_EXPIRES=3600,
 # )
-app = Celery('alerts',
-             include=alerts_include)
-app.config_from_object('celeryconfig', force=True)
+app = Celery("alerts", include=alerts_include)
+app.config_from_object("celeryconfig", force=True)
 
 # As a result of celery 3 to celery 4, we need to dynamically
 # register all of the alert tasks specifically
 for alert_namespace in CELERYBEAT_SCHEDULE:
     try:
-        alert_tokens = alert_namespace.split('.')
+        alert_tokens = alert_namespace.split(".")
         alert_module_name = alert_tokens[0]
         alert_classname = alert_tokens[1]
         alert_module = import_module(alert_module_name)
         alert_class = getattr(alert_module, alert_classname)
         app.register_task(alert_class())
     except ImportError as e:
-        print "Error importing {}".format(alert_namespace)
+        print ("Error importing {}").format(alert_namespace)
         print e
         pass
     except Exception as e:
-        print "Error addding alert"
+        print ("Error addding alert")
         print e
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.start()
