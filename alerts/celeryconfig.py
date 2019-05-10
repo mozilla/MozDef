6d05bc6650abed99afe57ea3a334d83f26e0d484
Andrew Krug <akrug@mozilla.com>
2019-05-10 08:55:02 -0700

    multiple fixes

--- alerts/celeryconfig.py
+++ alerts/celeryconfig.py
@@ -1,3 +1,4 @@
+import os
 from celery import Celery
 from importlib import import_module
 from lib.config import ALERTS, LOGGING, RABBITMQ
@@ -9,19 +10,27 @@ for alert in ALERTS.keys():
     alerts_include.append(".".join((alert).split(".")[:-1]))
 alerts_include = list(set(alerts_include))
 
-BROKER_URL = "amqp://{0}:{1}@{2}:{3}//".format(
-    RABBITMQ["mquser"], RABBITMQ["mqpassword"], RABBITMQ["mqserver"], RABBITMQ["mqport"]
-)
+# XXX TBD this should get wrapped into an object that provides pyconfig
+if os.getenv("OPTIONS_MQPROTOCOL", "amqp") == "sqs":
+    BROKER_URL = "sqs://@"
+    BROKER_TRANSPORT_OPTIONS = {'region': os.getenv('OPTIONS_ALERTSQSQUEUEURL').split('.')[1]}
+    CELERY_RESULT_BACKEND = None
+    CELERY_DEFAULT_QUEUE = os.getenv('OPTIONS_ALERTSQSQUEUEURL').split('/')[4]
+else:
+    BROKER_URL = "amqp://{0}:{1}@{2}:{3}//".format(
+        RABBITMQ["mquser"], RABBITMQ["mqpassword"], RABBITMQ["mqserver"], RABBITMQ["mqport"]
+    )
+    CELERY_QUEUES = {
+        "celery-default": {"exchange": "celery-default", "binding_key": "celery-default"}
+    }
+    CELERY_DEFAULT_QUEUE = 'celery-default'
+
 CELERY_DISABLE_RATE_LIMITS = True
 CELERYD_CONCURRENCY = 1
 CELERY_IGNORE_RESULT = True
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
-CELERY_DEFAULT_QUEUE = "celery-default"
-CELERY_QUEUES = {
-    "celery-default": {"exchange": "celery-default", "binding_key": "celery-default"}
-}
-
+CELERY_DEFAULT_QUEUE = 'celery-default'
 CELERYBEAT_SCHEDULE = {}
 
 # Register frequency of the tasks in the scheduler
@@ -61,11 +70,11 @@ for alert_namespace in CELERYBEAT_SCHEDULE:
         app.register_task(alert_class())
     except ImportError as e:
         print ("Error importing {}").format(alert_namespace)
-        print e
+        print(e)
         pass
     except Exception as e:
         print ("Error addding alert")
-        print e
+        print(e)
 
 if __name__ == "__main__":
     app.start()
