ce4b7b938862c36789db511670af85acd55bbb7e
Jonathan Claudius <jclaudius@mozilla.com>
2019-06-06 08:08:54 -0400

    Move config items to config file

--- alerts/ldap_password_spray.py
+++ alerts/ldap_password_spray.py
@@ -8,24 +8,21 @@
 
 from lib.alerttask import AlertTask
 from mozdef_util.query_models import SearchQuery, TermMatch
+import re
 
 
 class AlertLdapPasswordSpray(AlertTask):
     def main(self):
-        # TODO: evaluate what the threshold should be to detect a password spray
-        search_query = SearchQuery(minutes=60)
-
+        self.parse_config('ldap_password_spray.conf', ['threshold_count', 'search_depth_min'])
+        search_query = SearchQuery(minutes=int(self.config.search_depth_min))
         search_query.add_must([
             TermMatch('category', 'ldap'),
             TermMatch('tags', 'ldap'),
             TermMatch('details.response.error', 'LDAP_INVALID_CREDENTIALS')
         ])
-
         self.filtersManual(search_query)
         self.searchEventsAggregated('details.client', samplesLimit=10)
-
-        # TODO: evaluate what the threshold should be for this alert
-        self.walkAggregations(threshold=1)
+        self.walkAggregations(threshold=int(self.config.threshold_count))
 
     # Set alert properties
     def onAggregation(self, aggreg):
@@ -33,15 +30,37 @@ class AlertLdapPasswordSpray(AlertTask):
         tags = ['ldap']
         severity = 'WARNING'
 
-        user_dn_list = set()
+        email_list = set()
 
         for event in aggreg['allevents']:
             for request in event['_source']['details']['requests']:
-                user_dn_list.add(request['details'][0])
+                email = extractEmail(request['details'][0])
+                if email:
+                    email_list.add(extractEmail(request['details'][0]))
 
-        summary = 'Possible Password Spray Attack in Progress from {0} using the following distinguished names: {1}'.format(
+        # If no emails, don't throw alert
+        if len(email_list) == 0:
+            return None
+
+        summary = 'Password Spray Attack in Progress from {0} targeting the following accounts: {1}'.format(
             aggreg['value'],
-            ",".join(sorted(user_dn_list))
+            ",".join(sorted(email_list))
         )
 
         return self.createAlertDict(summary, category, tags, aggreg['events'], severity)
+
+    # A helper function to extract the first email in a string.
+    #
+    # Example:
+    #
+    # Input: 'dn="mail=user@example.com,o=com,dc=example"'
+    # Ouput: user@example.com
+    #
+    def extractEmail(string):
+        email_regex = r'mail=([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)'
+        match_object = re.match(email_regex, string)
+
+        if match_object:
+            return match_object.group(1)
+        else:
+            return None
