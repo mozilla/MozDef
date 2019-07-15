8379efb08ba19d0471439b8a59f35e46819c00ec
Emma Rose <emrose@mozilla.com>
2019-07-15 13:23:44 -0400

    Wrote tests for validator functions that will verify the correctness of the shape and values in a data model

---
+++ geomodel/tests/test_model.py
@@ -0,0 +1,144 @@
+import unittest
+
+import model 
+
+
+class TestValidateConfiguration(unittest.TestCase):
+    def test_valid_input(self):
+        assert model.validate_configuration({
+            'localities': {
+                'index': 'locality-state',
+                'validDurationHours': 32,
+                'radiusKilometres': 50.0,
+            },
+            'events': {
+                'index': 'events-*',
+                'queries': [
+                    {
+                        'lucene': 'tags:duo',
+                        'username': 'details.user'
+                    }
+                ]
+            },
+            'alerts': {
+                'index': 'alerts'
+            }
+        })
+
+
+    def test_invalid_input(self):
+        assert not model.validate_configuration({
+            'localities': {
+                'index': 'locality-state',
+                'validDurationHours': 32,
+                'radiusKilometres': 50.0,
+            },
+            'events': {
+                'index': 'events-*',
+                'queries': [
+                    {
+                        'lucene': 'tags:duo',
+                        'username': 'details.user'
+                    },
+                    {
+                        'lucne': 'tags:auth0',  # Key spelled wrong
+                        'username': 'details.user'
+                    }
+                ]
+            },
+            'alerts': {
+                'index': 'alerts'
+            }
+        })
+
+
+class TestValidateLocalityState(unittest.TestCase):
+    def test_valid_input(self):
+        assert model.validate_locality_state({
+            'username': 'testuser',
+            'localities': [
+                {
+                    'sourceipv4address': '122.32.50.109',
+                    'city': 'San Francisco',
+                    'country': 'USA',
+                    'lastaction': '2019-07-15 13:02:55.12362',
+                    'latitude': 37.773972,
+                    'longitude': -122.431297,
+                    'radius': 50.0
+                },
+                {
+                    'sourceipv4address': '55.13.62.50',
+                    'city': 'Toronto',
+                    'country': 'Canada',
+                    'lastaction': '2019-07-15 13:05:12.99123',
+                    'latitude': 43.70011,
+                    'longitude': -79.4163,
+                    'radius': 50.0
+                }
+            ]
+        })
+
+
+    def test_invalid_input(self):
+        assert not model.validate_locality_state({
+            'username': 'testuser',
+            'localities': [
+                {
+                    'sourceipv4address': '122.32.50.109',
+                    'city': 'San Francisco',
+                    'country': 'USA',
+                    'lastaction': '2019-07-15 13:02:55.12362',
+                    'latitude': -437.773972,  # Invalid latitude
+                    'longitude': -122.431297,
+                    'radius': 50.0
+                },
+                {
+                    'sourceipv4address': '55.13.62.50',
+                    'city': 'Toronto',
+                    'country': 'Canada',
+                    'lastaction': '2019-07-15 13:05:12.99123',
+                    'latitude': 43.70011,
+                    'longitude': -79.4163,
+                    'radius': 50.0
+                }
+            ]
+        })
+
+
+class TestValidateAlert(unittest.TestCase):
+    def test_valid_input(self):
+        assert model.validate_alert({
+            'source': 'geomodel',
+            'category': 'geomodel',
+            'type': 'geomodel',
+            'username': 'testuser',
+            'sourceipv4address': '12.39.55.252',
+            'timestamp': '2019-07-15 13:08:32.34123',
+            'origin': {
+                'city': 'San Francisco',
+                'country': 'USA',
+                'latitude': 37.773972,
+                'longitude': -122.431297
+            },
+            'tags': ['geomodel'],
+            'summary': 'test summary',
+        })
+
+
+    def test_invalid_input(self):
+        assert not model.validate_alert({
+            'source': 'geomodel',
+            'category': 'geomodel',
+            'type': 'geomodel',
+            'username': 'testuser',
+            'sourceipv4address': '1.329.55.257',  # Invalid IP
+            'timestamp': '2019-07-15 13:08:32.34123',
+            'origin': {
+                'city': 'San Francisco',
+                'country': 'USA',
+                'latitude': 37.773972,
+                'longitude': -122.431297
+            },
+            'tags': ['geomodel'],
+            'summary': 'test summary',
+        })
