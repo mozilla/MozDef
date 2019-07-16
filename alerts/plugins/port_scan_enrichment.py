bfa8640b576e2d6529dad239c6c2a7bfd84d84f1
Brandon Myers <bmyers@mozilla.com>
2019-07-16 16:12:56 -0400

    Modify registration for port scan alert plugin

--- alerts/plugins/port_scan_enrichment.py
+++ alerts/plugins/port_scan_enrichment.py
@@ -88,8 +88,8 @@ class message(object):
     '''
 
     def __init__(self):
-        # Run plugin on all alerts
-        self.registration = '*'
+        # Run plugin on portscan alerts
+        self.registration = 'portscan'
 
         config = _load_config(CONFIG_FILE)
 
