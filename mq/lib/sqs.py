8e1ec8f6061644265ca54f100ec304184a01bf47
Brandon Myers <bmyers@mozilla.com>
2019-07-18 15:59:21 -0400

    Fix region parameter in connect_sqs

--- mq/lib/sqs.py
+++ mq/lib/sqs.py
@@ -9,10 +9,16 @@ def connect_sqs(region_name=None, aws_access_key_id=None,
     if aws_secret_access_key is not None:
         credentials['aws_secret_access_key'] = aws_secret_access_key
 
+    additional_parameters = {
+        **credentials,
+    }
+
+    if region_name is not None:
+        additional_parameters['region_name'] = region_name
+
     sqs = boto3.resource(
         'sqs',
-        region_name=region_name,
-        **credentials
+        **additional_parameters
     )
     queue = sqs.get_queue_by_name(QueueName=task_exchange)
     return queue
