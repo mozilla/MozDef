34ee4cb18efafeb783084ab69c8985fd0ca6be4c
Brandon Myers <bmyers@mozilla.com>
2019-07-19 10:52:14 -0400

    Clean up connect_sqs function to call get_aws_credentials

--- mq/lib/sqs.py
+++ mq/lib/sqs.py
@@ -1,24 +1,12 @@
 import boto3
+from aws import get_aws_credentials
 
 
 def connect_sqs(region_name=None, aws_access_key_id=None,
                 aws_secret_access_key=None, task_exchange=None):
-    credentials = {}
-    if aws_access_key_id is not None:
-        credentials['aws_access_key_id'] = aws_access_key_id
-    if aws_secret_access_key is not None:
-        credentials['aws_secret_access_key'] = aws_secret_access_key
-
-    additional_parameters = {
-        **credentials,
-    }
-
-    if region_name is not None:
-        additional_parameters['region_name'] = region_name
-
     sqs = boto3.resource(
         'sqs',
-        **additional_parameters
+        **get_aws_credentials(region_name, aws_access_key_id, aws_secret_access_key)
     )
     queue = sqs.get_queue_by_name(QueueName=task_exchange)
     return queue
