e6a5a62e7712f659d08621bd0f94f3df8233c09f
Emma Rose <emrose@mozilla.com>
2019-07-18 20:16:47 -0400

    Implement types for locality state

--- geomodel/mozdef_geomodel/locality.py
+++ geomodel/mozdef_geomodel/locality.py
@@ -1,17 +1,55 @@
+from datetime import datetime
+from typing import List, NamedTuple
+
 from mozdef_util.query_models import SearchQuery, TermMatch
 
 import config
 import query
 
 
+# TODO: Switch to dataclasses when we move to Python3.7+
+
+class Locality(NamedTuple):
+    '''Represents a specific locality.
+    '''
+
+    sourceipv4address: str
+    city: str
+    country: str
+    lastaction: datetime
+    latitude: float
+    longitude: float
+    radius: int
+
+
+class State(NamedTuple):
+    '''Represents the state tracked for each user regarding their localities.
+    '''
+
+    type_: str
+    username: str
+    localities: List[Locality]
+
+
 def find_all(
         query_es: query.QueryInterface,
         locality: config.Localities
-        ) -> List[query.Event]:
+        ) -> List[State]:
     '''Retrieve all locality state from ElasticSearch.
     '''
+
+    def to_state(result: Dict[str, Any]) -> Optional[State]:
+        try:
+            return State(**result)
+        except TypeError:
+            return None
+
 
     search = SearchQuery()
     search.add_must([TermMatch('type', 'locality')])
 
-    return query_es(search, locality.esindex)
+    results = query_es(search, locality.es_index)
+
+    return list(filter(
+        lambda value: value is not None,
+        map(to_state, results)))
