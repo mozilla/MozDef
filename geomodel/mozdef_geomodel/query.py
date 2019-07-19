dd67487ef1988c22679025c4c26fb3a4a8dbbd42
Emma Rose <emrose@mozilla.com>
2019-07-19 14:49:20 -0400

    wrap/wrapper function type signature

--- geomodel/mozdef_geomodel/query.py
+++ geomodel/mozdef_geomodel/query.py
@@ -17,7 +17,7 @@ def wrap(client: ESClient) -> QueryInterface:
     '''Wrap an `ElasticsearchClient` in a closure of type `QueryInterface`.
     '''
     
-    def wrapper(query: SearchQuery, esindex: str) -> List[Event]:
+    def wrapper(query: SearchQuery, esindex: str) -> List[Dict[str, Any]]:
         return query.execute(client, indices=[esindex])
 
     return wrapper
