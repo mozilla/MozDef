7aa1cd6b3723b7244fe200eaab4f2f3ca51f47ae
Emma Rose <emrose@mozilla.com>
2019-07-23 16:26:46 -0400

    Trying to get tests to run in Docker

--- tests/mozdef_geomodel/test_event.py
+++ tests/mozdef_geomodel/test_event.py
@@ -1,13 +1,12 @@
-import sys
 from typing import Any, Dict, List
 import unittest
 
 from mozdef_util.query_models import SearchQuery
+from tests.unit_test_suite import UnitTestSuite
 
-sys.path.extend(['geomodel', 'geomodel/mozdef_geomodel'])
-import geomodel.mozdef_geomodel.config as config 
-import geomodel.mozdef_geomodel.event as event 
-import geomodel.mozdef_geomodel.query as query
+import mozdef_geomodel.config as config
+import mozdef_geomodel.event as event
+import mozdef_geomodel.query as query
 
 
 def _query_interface(results: List[Dict[str, Any]]) -> query.QueryInterface:
@@ -17,8 +16,51 @@ def _query_interface(results: List[Dict[str, Any]]) -> query.QueryInterface:
     def closure(q: SearchQuery, esi: str) -> List[Dict[str, Any]]:
         return results
 
-
     return closure
+
+
+class TestEventAgainstElasticSearch(UnitTestSuite):
+    def test_real_query_interface(self):
+        cfg = config.Events('events', config.SearchWindow(minutes=10), [
+            {
+                'lucene': 'category:test',
+                'username': 'details.username'
+            }
+        ])
+
+        events = [
+            {
+                'category': 'test',
+                'details': {
+                    'username': 'test1'
+                }
+            },
+            {
+                'category': 'test',
+                'details': {
+                    'username': 'test2'
+                }
+            },
+            {
+                'category': 'test',
+                'details': {
+                    'username': 'test3'
+                }
+            }
+        ]
+
+        for evt in events:
+            self.populate_test_object(evt)
+        self.refresh(cfg.es_index)
+
+        run_query = query.wrap(self.es_client)
+        results = event.find_all(run_query, cfg)
+        usernames = [res.username for res in results]
+
+        assert len(results) == 3
+        assert 'test1' in usernames
+        assert 'test2' in usernames
+        assert 'test3' in usernames
 
 
 class TestEvent(unittest.TestCase):
@@ -39,21 +81,20 @@ class TestEvent(unittest.TestCase):
             }
         ])
         evt_cfg = config.Events(
-                'test_index',
-                config.SearchWindow(minutes=30),
-                [
-                    config.QuerySpec(
-                        lucene='test',
-                        username='details.username')
-                ])
+            'test_index',
+            config.SearchWindow(minutes=30),
+            [
+                config.QuerySpec(
+                    lucene='test',
+                    username='details.username')
+            ])
 
         results = event.find_all(query_iface, evt_cfg)
         usernames = [result.username for result in results]
 
         assert 'testuser' in usernames
         assert 'test2' in usernames
-    
-    
+
     def test_find_all_handles_bad_username_spec(self):
         query_iface = _query_interface([
             {
@@ -68,20 +109,19 @@ class TestEvent(unittest.TestCase):
             }
         ])
         evt_cfg = config.Events(
-                'test_index',
-                config.SearchWindow(minutes=30),
-                [
-                    config.QuerySpec(
-                        lucene='test',
-                        username='details.nottheusername')  # Wrong key
-                ])
+            'test_index',
+            config.SearchWindow(minutes=30),
+            [
+                config.QuerySpec(
+                    lucene='test',
+                    username='details.nottheusername')  # Wrong key
+            ])
 
         results = event.find_all(query_iface, evt_cfg)
         usernames = [result.username for result in results]
 
         assert usernames == [None, None]
-    
-    
+
     def test_find_all_makes_all_requests(self):
         query_iface = _query_interface([
             {
@@ -96,16 +136,16 @@ class TestEvent(unittest.TestCase):
             }
         ])
         evt_cfg = config.Events(
-                'test_index',
-                config.SearchWindow(minutes=30),
-                [
-                    config.QuerySpec(
-                        lucene='test',
-                        username='details.username'),
-                    config.QuerySpec(
-                        lucene='another',
-                        username='details.nottheusername')
-                ])
+            'test_index',
+            config.SearchWindow(minutes=30),
+            [
+                config.QuerySpec(
+                    lucene='test',
+                    username='details.username'),
+                config.QuerySpec(
+                    lucene='another',
+                    username='details.nottheusername')
+            ])
 
         results = event.find_all(query_iface, evt_cfg)
         usernames = [result.username for result in results]
@@ -114,5 +154,3 @@ class TestEvent(unittest.TestCase):
         assert None in usernames
         assert 'testuser' in usernames
         assert 'test2' in usernames
-
-
