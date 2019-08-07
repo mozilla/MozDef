f1e3c11dec7c0bc1caf1a7ae590011d12c7cc94a
Brandon Myers <bmyers@mozilla.com>
2019-08-07 16:54:41 -0500

    Remove sys.path from mozdef_util tests

--- tests/mozdef_util/query_models/query_test_suite.py
+++ tests/mozdef_util/query_models/query_test_suite.py
@@ -5,12 +5,9 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 # Copyright (c) 2017 Mozilla Corporation
 
-import os
-import sys
 from mozdef_util.query_models import SearchQuery
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
-from unit_test_suite import UnitTestSuite
+from tests.unit_test_suite import UnitTestSuite
 
 
 class QueryTestSuite(UnitTestSuite):
