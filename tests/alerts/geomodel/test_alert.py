3d3bf06d09c02ed4ed637eac82aa6aa4f6591fb6
Emma Rose <emrose@mozilla.com>
2019-08-08 16:05:58 -0400

    Writing unit tests for an alert function that optionally produces an alert

---
+++ tests/alerts/geomodel/test_alert.py
@@ -0,0 +1,83 @@
+import unittest
+
+from alerts.geomodel.alert import alert
+import alerts.geomodel.config as config
+import alerts.geomodel.locality as locality
+
+
+class TestAlert(unittest.TestCase):
+    '''Unit tests for alert generation.
+    '''
+
+    def test_do_not_alert_on_whitelisted_user(self):
+        whitelist = config.Whitelist(users=['testuser1'], cidrs=[])
+        state = locality.State('locality', 'testuser1', [])
+
+        alert_produced = alert(state, whitelist)
+
+        assert alert_produced is None
+
+    def test_do_not_alert_on_whitelisted_ips(self)
+        whitelist = config.Whitelist(users=[], cidrs=['1.2.3.0/8'])
+        state = locality.State('locality', 'testuser', [
+            locality.Locality(
+                sourceipaddress='1.2.3.123',
+                city='Toronto',
+                country='CA',
+                lastaction=datetime.utcnow() - timedelta(minutes=5),
+                latitude=43.6529,
+                longitude=-79.3849)
+        ])
+
+        alert_produced = alert(state, whitelist)
+
+        assert alert_produced is None
+
+    def test_do_not_alert_when_travel_possible(self):
+        whitelist = config.Whitelist(users=[], cidrs=[])
+        state = locality.State('locality', 'testuser', [
+            locality.Locality(
+                sourceipaddress='1.2.3.123',
+                city='Toronto',
+                country='CA',
+                lastaction=datetime.utcnow() - timedelta(minutes=5),
+                latitude=43.6529,
+                longitude=-79.3849),
+            locality.Locality(
+                sourceipaddress='123.3.2.1',
+                city='San Francisco',
+                country='US',
+                lastaction=datetime.utcnow() - timedelta(hours=10),
+                latitude=37.773972,
+                longitude=-122.431297)
+        ])
+
+        alert_produced = alert(state, whitelist)
+
+        assert alert_produced is None
+
+    def test_do_alert_when_travel_impossible(self):
+        whitelist = config.Whitelist(users=[], cidrs=[])
+        state = locality.State('locality', 'testuser', [
+            locality.Locality(
+                sourceipaddress='1.2.3.123',
+                city='Toronto',
+                country='CA',
+                lastaction=datetime.utcnow() - timedelta(minutes=5),
+                latitude=43.6529,
+                longitude=-79.3849),
+            locality.Locality(
+                sourceipaddress='123.3.2.1',
+                city='San Francisco',
+                country='US',
+                lastaction=datetime.utcnow() - timedelta(hours=1),
+                latitude=37.773972,
+                longitude=-122.431297)
+        ])
+
+        alert_produced = alert(state, whitelist)
+
+        assert alert_produced is not None
+        assert alert_produced.username == 'testuser'
+        assert alert_produced.sourceipaddress == '1.2.3.123'
+        assert alert_produced.origin.city == 'Toronto'
