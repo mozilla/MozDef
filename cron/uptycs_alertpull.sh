fd55dbc788fdbbd78137e4e8688f1c8611cf51c8
Brandon Myers <bmyers@mozilla.com>
2019-09-06 12:33:07 -0500

    Create uptycs alertpull sh script

---
+++ cron/uptycs_alertpull.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# Copyright (c) 2014 Mozilla Corporation
+
+source  /opt/mozdef/envs/python/bin/activate
+/opt/mozdef/envs/mozdef/cron/uptycs_alertpull.py -c /opt/mozdef/envs/mozdef/cron/uptycs_alertpull.conf
