8407bba18a4289af4ab7d2050d9b0e5a3bf6995a
Brandon Myers <bmyers@mozilla.com>
2019-09-24 15:47:23 -0500

    Add geomodel test for multiple events in same window

--- tests/alerts/test_geomodel_location.py
+++ tests/alerts/test_geomodel_location.py
@@ -475,3 +475,86 @@ class TestSameCitiesOutsideRange(AlertTestSuite):
         if self.config_delete_indexes:
             self.es_client.delete_index('localities', True)
         super().teardown()
+
+
+class TestMultipleEventsInWindow(AlertTestSuite):
+    alert_filename = 'geomodel_location'
+    alert_classname = "AlertGeoModel"
+
+    default_event = {
+        "_source": {
+            "details": {
+                "sourceipaddress": "5.6.7.8",
+                "username": "tester1",
+                "sourceipgeolocation": {
+                    "city": "Toronto",
+                    "country_code": "CA",
+                    'latitude': 43.6529,
+                    'longitude': -79.3849,
+                },
+            },
+            "tags": ["auth0"],
+        }
+    }
+    outside_distance_event = {
+        "_source": {
+            "details": {
+                "sourceipaddress": "1.2.3.4",
+                "username": "tester1",
+                "sourceipgeolocation": {
+                    "city": "San Francisco",
+                    "country_code": "US",
+                    'latitude': 37.773972,
+                    'longitude': -122.431297,
+                },
+            },
+            "tags": ["auth0"],
+        }
+    }
+
+    default_alert = {
+        'category': 'geomodel',
+        'summary': 'tester1 is now active in San Francisco,US. Previously Toronto,CA',
+        'details': {
+            'username': 'tester1',
+            'sourceipaddress': '1.2.3.4',
+            'origin': {
+                'city': 'San Francisco',
+                'country': 'US',
+                'latitude': 37.773972,
+                'longitude': -122.431297,
+                'geopoint': '37.773972,-122.431297'
+            }
+        },
+        'severity': 'INFO',
+        'tags': ['geomodel']
+    }
+
+    events = [
+        AlertTestSuite.create_event(default_event),
+        AlertTestSuite.create_event(outside_distance_event)
+    ]
+    events[0]['_source']['utctimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 4})
+    events[0]['_source']['receivedtimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 4})
+    events[1]['_source']['utctimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 1})
+    events[1]['_source']['receivedtimestamp'] = AlertTestSuite.subtract_from_timestamp_lambda({'minutes': 1})
+    test_cases = [
+        PositiveAlertTestCase(
+            description='Does fire when search window contains 2 events with different locations',
+            events=events,
+            expected_alert=default_alert),
+    ]
+
+    @freeze_time("2017-01-01 01:00:00", tz_offset=0)
+    def setup(self):
+        super().setup()
+
+        index = 'localities'
+        if self.config_delete_indexes:
+            self.es_client.delete_index(index, True)
+            self.es_client.create_index(index)
+
+    def teardown(self):
+        if self.config_delete_indexes:
+            self.es_client.delete_index('localities', True)
+        super().teardown()
