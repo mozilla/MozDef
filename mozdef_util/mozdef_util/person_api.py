539ccbf6612c658de22c678f298589217b7267cb
Emma Rose <emrose@mozilla.com>
2019-11-08 16:34:35 -0500

    Sketching some code to fetch some of a user's profile from the Person API given their primary username

--- mozdef_util/mozdef_util/person_api.py
+++ mozdef_util/mozdef_util/person_api.py
@@ -3,7 +3,9 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 # Copyright (c) 2014 Mozilla Corporation
 
+from datetime import datetime
 from typing import Callable, NamedTuple, Optional
+from urllib.parse import urljoin
 
 import requests
 
@@ -21,9 +23,28 @@ class AuthParams(NamedTuple):
     grants: str
 
 
+class User(NamedTuple):
+    '''A container for information describing a user profile that is not
+    security critical.
+    '''
+
+    created: datetime
+    first_name: str
+    last_name: str
+    alternative_name: str
+    primary_email: str
+    mozilla_ldap_primary_email: str
+
+
+# We define some types to serve as 'interfaces' that can be referenced for
+# higher level functions and testing purposes.
+# This module defines implementations of each interface.
+
 Url = str
 Token = str
+Username = str
 AuthInterface = Callable[[Url, AuthParams], Optional[Token]]
+UserByNameInterface = Callable[[Url, Token, Username], Optional[User]]
 
 
 def authenticate(url: Url, params: AuthParams) -> Optional[Token]:
@@ -44,3 +65,50 @@ def authenticate(url: Url, params: AuthParams) -> Optional[Token]:
         return None
 
     return resp.json().get('access_token')
+
+
+def primary_username(base: Url, tkn: Token, uname: Username) -> Optional[User]:
+    '''An `UserByNameInterface` that uses the `requests` library to make a GET
+    request to the Person API in order to fetch a user profile given that
+    user's primary username.
+
+    The `base` argument is the base URL for the Person API such as
+    `https://person.api.com`.  This function will invoke the appropriate route.
+
+    `tkn` must be an authenticated session token produced by an `AuthInterface`.
+
+    `uname` is the string username of the user whose account to retrieve.
+    '''
+
+    route = '/v2/user/primary_username/{}'.format(uname)
+    full_url = urljoin(base, route)
+
+    headers = {
+        'Authorization': 'Bearer {}'.format(tkn)
+    }
+
+    try:
+        resp = requests.get(full_url, headers=headers)
+    except requests.exceptions.RequestException:
+        return None
+
+    data = resp.json()
+
+    try:
+        created = datetime.strptime(
+            data['created'].get('value', ''),
+            '%Y-%m-%dT%H:%M:%S.%fZ')
+    except ValueError:
+        return None
+
+    ldap_email = data['identities']['mozilla_ldap_primary_email'].get('value')
+    if ldap_email is None:
+        return None
+
+    return User(
+        created=created,
+        first_name=data['first_name'].get('value', 'N/A'),
+        last_name=data['last_name'].get('value', 'N/A'),
+        alternative_name=data['alternative_name'].get('value', 'N/A'),
+        primary_email=data['primary_email'].get('value', 'N/A'),
+        mozilla_ldap_primary_email=ldap_email)
