788684dbecacad45122237af28dbd3c7c040c8a3
Michal Purzynski <mpurzynski@mozilla.com>
2020-01-14 17:34:41 -0800

    removed unnecessary configuration options

--- mq/esworker_pubsub.py
+++ mq/esworker_pubsub.py
@@ -137,13 +137,6 @@ def initConfig():
 
     options.mqprotocol = getConfig("mqprotocol", "pubsub", options.configfile)
 
-    # plugin options
-    # secs to pass before checking for new/updated plugins
-    # seems to cause memory leaks..
-    # regular updates are disabled for now,
-    # though we set the frequency anyway.
-    options.plugincheckfrequency = getConfig("plugincheckfrequency", 120, options.configfile)
-
 
 def main():
     if hasUWSGI:
