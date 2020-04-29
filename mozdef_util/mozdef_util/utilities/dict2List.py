7ef21bc41cf279b9c207677d55707f254c8ba180
Emma Rose <682761+arcrose@users.noreply.github.com>
2020-04-29 14:24:49 -0400

    Fix geomodel alert and update mozdef-util (#1614)

--- mozdef_util/mozdef_util/utilities/dict2List.py
+++ mozdef_util/mozdef_util/utilities/dict2List.py
@@ -1,33 +1,21 @@
-def dict2List(inObj):
-    '''given a dictionary, potentially with multiple sub dictionaries
-       return a list of the dict keys and values
+from datetime import datetime
+
+
+def dict2List(value):
+    '''Convert dictionaries into a list of keys and values,
+    with strings in lowercase and datetimes converted to isoformat.
     '''
-    if isinstance(inObj, dict):
-        for key, value in inObj.items():
-            if isinstance(value, dict):
-                for d in dict2List(value):
-                    yield d
-            elif isinstance(value, list):
-                yield key.lower()
-                for l in dict2List(value):
-                    yield l
-            else:
-                yield key.lower()
-                if isinstance(value, str):
-                    yield value.lower()
-                else:
-                    yield value
-    elif isinstance(inObj, list):
-        for v in inObj:
-            if isinstance(v, str):
-                yield v.lower()
-            elif isinstance(v, list):
-                for l in dict2List(v):
-                    yield l
-            elif isinstance(v, dict):
-                for l in dict2List(v):
-                    yield l
-            else:
-                yield v
+
+    if isinstance(value, dict):
+        for key, val in value.items():
+            yield from dict2List(key)
+            yield from dict2List(val)
+    elif isinstance(value, (list, tuple)):
+        for val in value:
+            yield from dict2List(val)
+    elif isinstance(value, datetime):
+        yield value.isoformat()
+    elif isinstance(value, str):
+        yield value.lower()
     else:
-        yield ''
+        yield value
--- mozdef_util/setup.py
+++ mozdef_util/setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=[],
     url='https://github.com/mozilla/MozDef/tree/master/lib',
-    version='3.0.4',
+    version='3.0.5',
     zip_safe=False,
 )
