3fbd959ffdc098ff891f79d863ed48dfc758a55d
Brandon Myers <pwnbus@users.noreply.github.com>
2020-05-08 13:37:21 -0500

    Remove unused alerts (#1625)

--- docker/compose/mozdef_alerts/files/config.py
+++ docker/compose/mozdef_alerts/files/config.py
@@ -12,7 +12,6 @@ import os
 
 ALERTS = {
     'bruteforce_ssh.AlertBruteforceSsh': {'schedule': crontab(minute='*/1')},
-    'unauth_ssh.AlertUnauthSSH': {'schedule': crontab(minute='*/1')},
     'get_watchlist.AlertWatchList': {'schedule': crontab(minute='*/1')},
 }
 
