1508a541be2fbf01917f2a02d6aab5cb35a2a699
Phrozyn <asmith@mozilla.com>
2020-06-30 14:14:04 -0500

    remove dots from the start or end of a key

---
+++ mq/plugins/remove_dots.py
@@ -0,0 +1,33 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# Copyright (c) 2014 Mozilla Corporation
+
+import json
+
+
+class message(object):
+
+    def __init__(self):
+        '''
+        takes an incoming message
+        and sets the keys to lowercase
+        '''
+
+        self.registration = ['cloudtrail']
+        self.priority = 5
+
+    def onMessage(self, message, metadata):
+        def remove_dots(obj):
+            for key in obj.keys():
+                if key[0] == '.' or key[-1] == '.':
+                    new_key = key.replace(".","")
+                    if new_key != key:
+                        obj[new_key] = obj[key]
+                        del obj[key]
+            return obj
+
+        new_json = json.loads(json.dumps(message), object_hook=remove_dots)
+        message = new_json
+
+        return (message, metadata)
